6c65d259c53efd5fc4c9ca64c3147cf580e85bf8
Gabriel Perdue <perdue@fnal.gov>
2009-12-29 18:07:44 -0600

    Basic script to do quick HV checks during light leak checkout.

---
+++ mnvconfigurator/basic/hvCheck/runner.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+gmake clean
+gmake
+
+if [ $# -gt 0 ]; then
+ if [ $1 == "v" ]; then
+  valgrind -v --log-file-exactly=valtest1.txt ./hvCheck -c 1
+ fi
+else
+./hvCheck -c 1
+fi
+
+exit 0
