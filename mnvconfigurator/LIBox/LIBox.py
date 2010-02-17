1519f425bb2ec31152acc923c2010d18d7c6c548
minervacvs <minervacvs>
2010-02-17 00:47:51 +0000

    Sundry small bugfixes.

--- mnvconfigurator/LIBox/LIBox.py
+++ mnvconfigurator/LIBox/LIBox.py
@@ -12,6 +12,9 @@ import time
 import re
 import sys
 
+# for testing purposes.
+LIBOX_WAIT_FOR_RESPONSE = True
+
 # configuration for the serial port.
 # these probably will never need to be changed.
 LIBOX_SERIAL_PORT_DEVICE         = "/dev/ttyS0"
@@ -41,7 +44,7 @@ class LIBox:
 		# this is probably not strictly necessary since 
 		# I can't imagine a user ever setting the commands by hand,
 		# but it's usually better to be safe anyway.
-		self.permitted_commands = ( "_X" , "aA", "aE[0a-vA-V]", "aB[0-3]", "aC[\da-eA-E]_[\da-eA-E]", "aO", "aD[0-7]", "aQ", "aK", "aH[\da-eA-E]{2}", "aI[\da-eA-E]{2}" )
+		self.permitted_commands = ( "_X" , "aA", "aE[0a-vA-V]", "aB[0-3]", "aC[\da-fA-F]_[\da-fA-F]", "aO", "aD[0-7]", "aQ", "aK", "aH[\da-fA-F]{2}", "aI[\da-fA-F]{2}" )
 
 		# this will throw an exception if the port's not properly configured.
 		# that's ok -- it needs to be fixed in that case!
@@ -54,7 +57,8 @@ class LIBox:
 		                           xonxoff  = LIBOX_SERIAL_PORT_SW_FLOWCONTROL,
 		                           rtscts   = LIBOX_SERIAL_PORT_HW_FLOWCONTROL,
 		                           timeout  = 0.1 )
-	
+
+		self.port.writeTimeout = 0.1
 		
 	def reset(self):
 		self.command_stack = ["_X"]
@@ -72,13 +76,21 @@ class LIBox:
 		
 		for command in self.command_stack:
 			self.command_log.append(command)		# store this in a log for later review.
-			self.port.write(command)
-			char = self.read(1)
+			try:
+#				print "Writing command '" + command + "'..."
+				self.port.write(command + "\n")
+			except serial.SerialTimeoutException:
+				raise LIBoxException("The LI box isn't responding.  Are you sure you have the correct port setting and that the LI box is on?")
+
+			if LIBOX_WAIT_FOR_RESPONSE:
+				char = self.port.read(1)
 			
-			if char != "K":
-				raise LIBoxException("The LI box didn't respond affirmatively to the command: '" + command + "'.")
+				if char != "K":
+					raise LIBoxException("The LI box didn't respond affirmatively to the command: '" + command + "'.")
 			
 			time.sleep(0.01)
+		
+		self.command_stack = []
 			
 	def check_commands(self):
 		for command in self.command_stack:
@@ -96,23 +108,25 @@ class LIBox:
 		
 		self.command_stack = []
 		
-		if not(self.initialized):
+		if not self.initialized:
 			self.initialize()
 		
 		# first, LED group.
 		self.LED_groups = self.LED_groups.upper()
 		
-		if not(re.match("[a-dA-D]{1-4}"), self.LED_groups):
+		if not re.match("[a-dA-D]{1,4}", self.LED_groups):
 			raise LIBoxException("LED groups to use must be some combination of 'A', 'B', 'C', 'D'.")
-		LEDcodes = "0abcdefghijklmnopqrstuv";
 
 		# the following wizardry is brought to you by the magic that powers the LI box.
 		# trust me -- it works.
 		inverseAddress = 0
+		pos = 0
+		LEDcodes = "0abcdefghijklmnopqrstuv";
 		for char in "ABCD":
-			inverseAddress += 2 ** (self.LED_groups.count(char))
-
+			inverseAddress += self.LED_groups.count(char) * 2 ** (pos)
+			pos += 1
 		LEDcode = LEDcodes[15 - inverseAddress];
+
 		self.command_stack.append("aE" + LEDcode)
 		
 		
@@ -122,14 +136,16 @@ class LIBox:
 			raise LIBoxException("LI pulse height must be between 4.05 and 12.07 volts (inclusive).")
 		
 		highBit = int( (self.pulse_height - 4.0429) / 2.01 )
-		highBit = highBit.hex().replace("0x", "")
-		self.command_stack.append("aB" + highBit)
 
 		lowBit  = int( (self.pulse_height - highBit * 2.01 - 4.0429) / .00783 )
 		lowBit1 = lowBit / 16;		# note: INTEGER division.
-		lowBit1 = lowBit1.hex().replace("0x", "")
 		lowBit2 = lowBit % 16;
-		lowBit2 = lowBit2.hex().replace("0x", "")
+
+		highBit = "%x" % highBit
+		self.command_stack.append("aB" + highBit)
+
+		lowBit1 = "%x" % lowBit1
+		lowBit2 = "%x" % lowBit2
 		self.command_stack.append("aC" + lowBit1 + "_" + lowBit2)
 		
 		# must always follow "aB" and "aC" with "aO"...
@@ -147,25 +163,25 @@ class LIBox:
 				
 			self.command_stack.append("aK")
 
-			self.trigger_rate = int(self.trigger_rate)		# no decimals.
+			self.trigger_rate = int(self.trigger_rate, 16)		# no decimals.  need it in hexadecimal, too.
 			if self.trigger_rate < 0 or self.trigger_rate > 0xffff:
 				raise LIBoxException("Internal trigger rate must be between 0 and FFFF (hex).")
 			
 			trigger_rate = self.trigger_rate
 			
 			highNum1 = trigger_rate / 0x1000
-			highNum2 = (trigger_rate - highNum1) / 0x100
-			trigger_rate -= highNum1 + highNum2
+			highNum2 = (trigger_rate - highNum1 * 0x1000) / 0x100
+			trigger_rate -= highNum1 * 0x1000 + highNum2 * 0x100
 			
-			highNum1 = highNum1.hex().replace("0x", "")
-			highNum2 = highNum2.hex().replace("0x", "")
-			command_stack.append("aH" + highNum1 + highNum2)
+			highNum1 = "%x" % highNum1
+			highNum2 = "%x" % highNum2
+			self.command_stack.append("aH" + highNum1 + highNum2)
 			
 			lowNum1 = trigger_rate / 0x10
-			lowNum2 = trigger_rate - lowNum1
-			lowNum1 = lowNum1.hex().replace("0x", "")
-			lowNum2 = lowNum2.hex().replace("0x", "")
-			command_stack.append("aI" + lowNum1 + lowNum2)
+			lowNum2 = trigger_rate - lowNum1 * 0x10
+			lowNum1 = "%x" % lowNum1
+			lowNum2 = "%x" % lowNum2
+			self.command_stack.append("aI" + lowNum1 + lowNum2)
 		else:
 			self.command_stack.append("aQ")
 		
@@ -182,11 +198,11 @@ if __name__ == "__main__":
 	import optparse
 	
 	parser = optparse.OptionParser(usage="usage: %prog [options] command\n  where 'command' is 'initialize', 'reset', or 'configure'")
-	parser.add_option("-p", "--pulse-height", dest="pulse_height", help="Set the pulse height (volts).  Valid values: 4.05-12.07.  Default: %default.", default=12.07)
-	parser.add_option("-w", "--pulse-width", dest="pulse_width", help="Set the pulse width.  Valid values: 0-7 (roughly corresponding to 20-35 ns).  Default: %default.", default=7)
+	parser.add_option("-p", "--pulse-height", dest="pulse_height", help="Set the pulse height (volts).  Valid values: 4.05-12.07.  Default: %default.", default=12.07, type="float")
+	parser.add_option("-w", "--pulse-width", dest="pulse_width", help="Set the pulse width.  Valid values: 0-7 (roughly corresponding to 20-35 ns).  Default: %default.", default=7, type="int")
 	parser.add_option("-l", "--led-groups", dest="led_groups", help="Set the LED groups that will fire.  Valid values: some combination of 'A', 'B', 'C', 'D'.  Default: %default.", default="ABCD")
-	parser.add_option("-i", "--trigger-internal", dest="trigger_internal", action="store_true", help="Use internal triggering for the LI pulses.  (You should also set -r if you use this option.)  Default: %default", default=False)
-	parser.add_option("-r", "--trigger-rate", dest="trigger_rate", help="Set the triggering rate for internal triggering.  (You should only use this option in conjunction with -i.)  Default: None.", default=None)
+	parser.add_option("-i", "--trigger-internal", dest="trigger_internal", action="store_true", help="Use internal triggering for the LI pulses instead of the usual external trigger from the pulser.  (You should also set -r if you use this option.)  Default: %default", default=False)
+	parser.add_option("-r", "--trigger-rate", dest="trigger_rate", help="Set the triggering rate for internal triggering.  (You should only use this option in conjunction with -i.)  Valid values: 0-FFFF (hex).  Default: None.", default=None)
 	
 	(options, commands) = parser.parse_args()
 	
@@ -195,6 +211,8 @@ if __name__ == "__main__":
 		sys.exit(0)
 
 	command = commands[0].lower()
+	
+#	print options
 	
 	if command == "initialize":
 		box = LIBox()
