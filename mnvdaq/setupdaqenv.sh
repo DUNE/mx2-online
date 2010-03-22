f221cb0e3f501f890680280d358f8fe37034c683
Arturo Fiorentini Aguirre <arturo@fnal.gov>
2010-03-22 11:53:31 -0500

    Added support for Nearline station.

--- mnvdaq/setupdaqenv.sh
+++ mnvdaq/setupdaqenv.sh
@@ -23,6 +23,16 @@ then
 	export LD_LIBRARY_PATH=$DAQROOT/lib:$ET_LIBROOT/lib:$CAEN_DIR/lib/x86_64/:$LD_LIBRARY_PATH
 	# Add /usr/local/lib for log4cpp support.
 	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
+elif [ $LOCALE == 'NEARLINE' ]
+then
+	export DAQROOT=/scratch/nearonline/mnvdaq
+	export CAEN_DIR=/scratch/nearonline/CAENVMElib
+	export ET_HOME=$DAQROOT/et_9.0/Linux-x86_64-64
+	export ET_LIBROOT=$ET_HOME/Linux-x86_64-64
+	# Add $ET_LIBROOT/lib & $CAEN_DIR/lib for ET & CAEN libraries.
+	export LD_LIBRARY_PATH=$DAQROOT/lib:$ET_LIBROOT/lib:$CAEN_DIR/lib/x86_64/:$LD_LIBRARY_PATH
+	# Add log4cpp support.
+	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/scratch/nearonline/log4cpp/lib
 else
 	echo Unsupported LOCALE!
 	exit 1
