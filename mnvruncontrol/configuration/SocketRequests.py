6bdcbc8dc1abb9e0615915e9f7146d95497e947a
cvs-fast-export <cvs-fast-export>
2010-03-23 23:52:03 +0000

    Synthetic commit for incomplete tag v6r2

--- mnvruncontrol/configuration/SocketRequests.py
+++
@@ -1,8 +0,0 @@
-ValidRequests = ( "(?P<request>alive)\?",
-                  "(?P<request>daq_running)\?",
-                  "(?P<request>daq_last_exit)\?",
-                  "(?P<request>daq_start) etfile=(?P<etfile>\S+):etport=(?P<etport>\d+):run=(?P<run>\d+):subrun=(?P<subrun>\d+):gates=(?P<gates>\d+):runmode=(?P<runmode>\d+):detector=(?P<detector>\d+):nfebs=(?P<nfebs>\d+):lilevel=(?P<lilevel>\d+):ledgroup=(?P<ledgroup>\d+):hwinitlevel=(?P<hwinitlevel>\d+):identity=(?P<identity>(\w+))!",
-                  "(?P<request>daq_stop)!",
-                  "(?P<request>sc_setHWconfig) (?P<filename>\S+)!",
-                  "(?P<request>sc_voltages)\?" )
-
