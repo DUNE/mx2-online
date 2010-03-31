f2df75ab872969870a50a8401be771df0b481837
Jeremy Wolcott <jwolcott@fnal.gov>
2010-03-31 14:49:34 -0400

    * moved one PE, max PE voltages here * lengthened socket timeout

--- mnvruncontrol/configuration/Defaults.py
+++ mnvruncontrol/configuration/Defaults.py
@@ -15,10 +15,6 @@ import time
 EVENT_SIZE = 2048 
 FRAMES = 8
 
-# LI settings
-LI_ONE_PE_VOLTAGE = 5.07		# from Brandon Eberly (eberly@fnal.gov), 3/5/2010
-LI_MAX_PE_VOLTAGE = 12.07	# currently the maximum the LI box can output.
-
 # Run control properties.
 CONFIG_DB_LOCATION = "/work/conditions/run_control_config.db"
 
@@ -44,9 +40,9 @@ WORKER  = "mnvonline1.fnal.gov"
 MASTER = "localhost"
 MONITOR = "mnvnearline1.fnal.gov"
 
-MAX_CONNECTION_ATTEMPTS = 3
+MAX_CONNECTION_ATTEMPTS = 5
 CONNECTION_ATTEMPT_INTERVAL = 0.5	# in seconds
-SOCKET_TIMEOUT = 2.5  # in seconds
+SOCKET_TIMEOUT = 0.5  # in seconds
 
 # number of times in a row to log the same request
 # in the dispatcher before suppressing
