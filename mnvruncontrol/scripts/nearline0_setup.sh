d214ff688266b2ddb6d76e9ff8dcc3d0270e9fad
Jeremy Wolcott <jwolcott@fnal.gov>
2010-04-04 15:53:58 -0400

    Minor configuration updates

--- mnvruncontrol/scripts/nearline0_setup.sh
+++ mnvruncontrol/scripts/nearline0_setup.sh
@@ -4,7 +4,11 @@ export UTGID=NEARONLINE
 
 source /work/mnvdaq/setupdaqenv.sh
 source /home/nearonline/cmtuser/setup.sh
-source /work/mnvsoft/v7r2/minerva/MINERVA/MINERVA_v7r2/DaqRecv/cmt/setup.sh
+pushd /work/mnvsoft/v7r2/minerva/MINERVA/MINERVA_v7r2/DaqRecv/cmt
+source ./setup.sh
+popd
 
 export PATH=/work/python/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/work:$PYTHONPATH
 
