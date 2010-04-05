670d0dc22b1fffeb408d0e7b6c5eee71ff39ebd4
Jeremy Wolcott <jwolcott@fnal.gov>
2010-04-05 14:50:08 -0400

    Split config DB file path into name and location

--- mnvruncontrol/configuration/Defaults.py
+++ mnvruncontrol/configuration/Defaults.py
@@ -20,7 +20,8 @@ LI_ONE_PE_VOLTAGE = 5.07		# from Brandon Eberly (eberly@fnal.gov), 3/5/2010
 LI_MAX_PE_VOLTAGE = 12.07	# currently the maximum the LI box can output.
 
 # Run control properties.
-CONFIG_DB_LOCATION = "/work/conditions/run_control_config.db"
+CONFIG_DB_NAME = "run_control_config.db"
+CONFIG_DB_LOCATION = "/work/conditions"
 
 RUN_SERIES_DB_LOCATION_DEFAULT = "/work/conditions/run_series"
 RUN_SUBRUN_DB_LOCATION_DEFAULT = "/work/conditions/next_run_subrun.db"
