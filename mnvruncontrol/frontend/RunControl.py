3c46fc4ff8dbf9611557cfb4b2868745d87eb144
Jeremy Wolcott <jwolcott@fnal.gov>
2010-04-06 16:02:09 -0400

    Fixed some log file sorting mistakes

--- mnvruncontrol/frontend/RunControl.py
+++ mnvruncontrol/frontend/RunControl.py
@@ -853,12 +853,12 @@ class MainFrame(wx.Frame):
 		f2 = int(fileinfo2["run"])*10000 + int(fileinfo2["subrun"])
 		
 		if f1 == f2:
-			t1 = time.strptime("2010 " + fileinfo1["date"], "%Y %H:%M")		# need to include a year because otherwise the 'mktime' below overflows.  which year it is is irrelevant (all we need is a difference anyway).
-			t2 = time.strptime("2010 " + fileinfo2["date"], "%Y %H:%M")
+			t1 = time.strptime("2010 " + fileinfo1["time"], "%Y %H:%M")		# need to include a year because otherwise the 'mktime' below overflows.  which year it is is irrelevant (all we need is a difference anyway).
+			t2 = time.strptime("2010 " + fileinfo2["time"], "%Y %H:%M")
 			
 			timediff = time.mktime(t1) - time.mktime(t2)
 			if timediff == 0:		# this should never happen.
-				return 0
+				return 1 if fileinfo1["controller"] > fileinfo2["controller"] else -1
 			else:
 				return 1 if timediff > 0 else -1
 		else:
@@ -878,10 +878,13 @@ class MainFrame(wx.Frame):
 			if matchdata1.group("subrun") == matchdata2.group("subrun"):		# shouldn't ever have same run/subrun combination
 				if matchdata1.group("hour") == matchdata2.group("hour"):
 					if matchdata1.group("minute") == matchdata2.group("minute"):
-						print "Run/subrun pair equal! : (", matchdata1.group("run"), ",", matchdata1.group("subrun"), ") == (", matchdata2.group("run"), ",", matchdata2.group("subrun"), ")!"
-						print "Files: '" + file1 + "', '" + file2 + "'"
-						print "Log sorting problem."
-						sys.exit(1)
+						if matchdata1.group("controller") == matchdata2.group("controller"):
+							print "Run/subrun pair equal! : (", matchdata1.group("run"), ",", matchdata1.group("subrun"), ") == (", matchdata2.group("run"), ",", matchdata2.group("subrun"), ")!"
+							print "Files: '" + file1 + "', '" + file2 + "'"
+							print "Log sorting problem."
+							sys.exit(1)
+						else:
+							return 1 if matchdata1.group("controller") > matchdata2.group("controller") else -1
 					else:
 						return 1 if matchdata1.group("minute") > matchdata2.group("minute") else -1
 				else:
