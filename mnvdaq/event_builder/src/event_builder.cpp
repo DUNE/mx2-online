8cffc3a69d67b014e94f753f4456734985681b88
Gabriel Perdue <perdue@fnal.gov>
2010-04-07 12:52:44 -0500

    Clarify error message.

--- mnvdaq/event_builder/src/event_builder.cpp
+++ mnvdaq/event_builder/src/event_builder.cpp
@@ -158,7 +158,8 @@ int main(int argc, char **argv)
 
 	// Request an event from the ET service.
 	std::cout << "Starting!" << std::endl;
-	std::cout << "(If 20 seconds goes by and the DAQ doesn't start, please stop and try again.)" << std::endl;
+	std::cout << "If 20 seconds goes by and the DAQ doesn't start, " << std::endl;
+	std::cout << "please skip to the next subrun or stop and try again." << std::endl;
 	int evt_counter = 0;
 	while ((et_alive(sys_id))) {
 		struct timespec time;
