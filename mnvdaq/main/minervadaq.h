4120b822c1cfee771a09b034f0a4f0b07d9b0b2f
Gabriel Perdue <perdue@fnal.gov>
2010-04-17 15:17:00 -0500

    Working commit.  Will not compile right now...

--- mnvdaq/main/minervadaq.h
+++ mnvdaq/main/minervadaq.h
@@ -17,9 +17,9 @@ int TriggerDAQ(acquire_data *daq, unsigned short int triggerType, RunningModes r
 /*! the function which governs the entire data acquisition sequence */
 int TakeData(acquire_data *daq, event_handler *evt, int croc_id, int channel_id,int thread, 
               et_att_id  attach, et_sys_id  sys_id, bool readFPGA=true, int nReadoutADC=8); 
-/*! Get the Global Gate value indexed in /work/conditions/global_gate.dat */
+/*! Get the Global Gate value indexed in /work/conditions/soldierToWorker.dat */
 int inline GetGlobalGate();
-/*! Put the Global Gate value into /work/conditions/global_gate.dat */
+/*! Put the Global Gate value into /work/conditions/soldierToWorker.dat */
 void inline PutGlobalGate(int ggate);
 /*! Write the (complete, as of trigger X) SAM metadata */
 int WriteSAM(const char samfilename[], 
@@ -41,44 +41,44 @@ std::ofstream take_data_extime_log; /*!<an output file for tiing data */
 std::ofstream trigger_log; /*!<an output file for trigger debuggin */
 
 // Socket Communication Functions
-void CreateSocketPair(int &gate_done_socket_handle, int &global_gate_socket_handle);
+void CreateSocketPair(int &workerToSoldier_socket_handle, int &soldierToWorker_socket_handle);
 void SetupSocketService(struct sockaddr_in &socket_service, struct hostent *node_info, 
 	std::string hostname, const int port );
 
 // Socket Communication Vars.
-bool               gate_done[1]; // signal for end of event readout completion from the worker -> soldier
-unsigned long long global_gate_data[1]; // signal for end of trigger from the worker -> soldier
-bool               ready_to_go[1];
+bool               gate_done_data[1];       // end of gate complete signal from the worker -> soldier 
+unsigned short int workerToSoldier_trig[1]; // trigger check the worker -> soldier 
+unsigned short int soldierToWorker_trig[1]; // trigger check the soldier -> worker 
 
-// End of Gate service vars.
-struct in_addr     gate_done_socket_address;
-int                gate_done_socket_connection; 
-bool               gate_done_socket_is_live;
+// worker to soldier service vars.
+struct in_addr     workerToSoldier_socket_address;
+int                workerToSoldier_socket_connection; 
+bool               workerToSoldier_socket_is_live;
 
-// Global Gate service vars.
-struct in_addr     global_gate_socket_address;
-int                global_gate_socket_connection; 
-bool               global_gate_socket_is_live;
+// soldier to worker service vars.
+struct in_addr     soldierToWorker_socket_address;
+int                soldierToWorker_socket_connection; 
+bool               soldierToWorker_socket_is_live;
 
 
-// TODO - cycle between a set of ports
-unsigned short gate_done_port   = 1110;
-unsigned short global_gate_port = 1120; 
+// Base values for cycle between set of ports.
+unsigned short workerToSoldier_port = 1110;
+unsigned short soldierToWorker_port = 1120; 
 
 #if MASTER&&(!SINGLEPC) // Soldier Node
 /* minervadaq server for "master" (soldier node) DAQ */
-struct sockaddr_in          gate_done_service;
-struct sockaddr_in          global_gate_service;
-int                         gate_done_socket_handle;
-int                         global_gate_socket_handle;
+struct sockaddr_in          workerToSoldier_service;
+struct sockaddr_in          soldierToWorker_service;
+int                         workerToSoldier_socket_handle;
+int                         soldierToWorker_socket_handle;
 struct hostent *            worker_node_info; // server on worker node
 #endif
 
 #if (!MASTER)&&(!SINGLEPC) // Worker Node
 /* minervadaq client for "slave" (worker node) DAQ */
-struct sockaddr_in          gate_done_service;
-struct sockaddr_in          global_gate_service;
-int                         gate_done_socket_handle;
-int                         global_gate_socket_handle;
+struct sockaddr_in          workerToSoldier_service;
+struct sockaddr_in          soldierToWorker_service;
+int                         workerToSoldier_socket_handle;
+int                         soldierToWorker_socket_handle;
 struct hostent *            soldier_node_info; // server on soldier node
 #endif
