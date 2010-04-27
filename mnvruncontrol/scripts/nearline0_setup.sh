7eeb638f3b2b1ff92e886b309f866adbf41e6cf1
Jeremy Wolcott <jwolcott@fnal.gov>
2010-04-27 16:51:03 -0400

    Use cmtuser directory for setup

--- mnvruncontrol/scripts/nearline0_setup.sh
+++ mnvruncontrol/scripts/nearline0_setup.sh
@@ -1,10 +1,9 @@
 export LOCALE=NEARLINE
 export DIM_DNS_NODE=mnvnearline1.fnal.gov
-export UTGID=NEARONLINE
 
 source /work/mnvdaq/setupdaqenv.sh
 source /home/nearonline/cmtuser/setup.sh
-pushd /work/mnvsoft/v7r2/minerva/MINERVA/MINERVA_v7r2/DaqRecv/cmt
+pushd /home/nearonline/cmtuser/Minerva_v7r2/DaqRecv/cmt/
 source ./setup.sh
 popd
 
--- mnvruncontrol/scripts/nearline1_setup.sh
+++ mnvruncontrol/scripts/nearline1_setup.sh
@@ -1,14 +1,15 @@
 export LOCALE=NEARLINE
 export DIM_DNS_NODE=mnvnearline1.fnal.gov
-export UTGID=NEARONLINE
 
 source /scratch/nearonline/mnvdaq/setupdaqenv.sh
-source /home/nearonline/cmtuser/setup.sh
-pushd /scratch/nearonline/software_releases/v7r2/minerva/MINERVA/MINERVA_v7r2/DaqRecv/cmt
+
+#### ATTENTION: this script needs to be updated for new framework versions!!!
+source /scratch/nearonline/software_releases/v7r2/setup.sh
+pushd /home/nearonline/cmtuser/Minerva_v7r2/DaqRecv/cmt/
 source ./setup.sh
 popd
 
-export PATH=/scratch/nearonline/python/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=/scratch/nearonline:$PYTHONPATH
+export PATH=/scratch/nearonline/python/bin:$PATH		# newer version of Python
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH		# for log4cpp
+export PYTHONPATH=/scratch/nearonline:$PYTHONPATH		# so that mnvruncontrol shows up as a package in Python
 
