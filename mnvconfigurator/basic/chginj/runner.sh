80568ae9491e4be73fff08b6d4d0426323d9f3f3
Gabriel Perdue <perdue@fnal.gov>
2010-08-10 09:33:42 -0500

    Updated for more machine flexibility.

--- mnvconfigurator/basic/chginj/runner.sh
+++ mnvconfigurator/basic/chginj/runner.sh
@@ -3,11 +3,20 @@
 gmake clean
 gmake
 
+if [ $HOSTNAME == "minervatest02.fnal.gov" ]; then
+#WH14T
+	./chginj -c 1 -h 1 -f 1
+	./chginj -c 1 -h 2 -f 1
+	./chginj -c 1 -h 3 -f 1
+fi
+
+if [ $HOSTNAME == "minervatest04.fnal.gov" ]; then
 #WH14B
-./chginj -c 1 -h 1 -f 1 
-./chginj -c 1 -h 2 -f 2
-./chginj -c 1 -h 3 -f 1
-./chginj -c 5 -h 4 -f 2
-echo " "
-#echo "croc 5, most of 1 commented out now..."
+	./chginj -c 1 -h 1 -f 1 
+	./chginj -c 1 -h 2 -f 2
+	./chginj -c 1 -h 3 -f 1
+	./chginj -c 5 -h 4 -f 2
+	echo " "
+	#echo "croc 5, most of 1 commented out now..."
+fi
 
