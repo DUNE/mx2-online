fcb7d8cb0dc4421d5d84559aab26ea3d0e869fd3
Gabriel Perdue <perdue@fnal.gov>
2010-08-11 10:42:28 -0500

    Working commit - keeping up with changes for FPGA "DumpRead."

--- mnvdaq/fermihardware/src/feb.cpp
+++ mnvdaq/fermihardware/src/feb.cpp
@@ -32,23 +32,24 @@ feb::feb(int mh, bool init, febAddresses a, int reg, log4cpp::Appender* appender
 	febAppender  = appender; // log4cpp appender
 	if (febAppender!=0) febLog.setPriority(log4cpp::Priority::ERROR);
 	
-	//if ( (maxHits != 1) && (maxHits != 6) ) {
-	//	std::cout << "Invalid number of maximum hits!  Only 1 or 6 are accepted right now!" << std::endl;
-	//	if (febAppender!=0) febLog.fatalStream() << "Invalid number of maximum hits!  Only 1 or 6 are accepted right now!";
-	//	exit(-1);
-	//}
-
 	// Make the header for this frame; frames default to read. 
 	Devices dev = FPGA;     //the device type for the header
 	Broadcasts b = None;    //broadcast type for header
 	Directions d = MasterToSlave; //message direction for header
-	FPGAFunctions f = Read; //operation to be performed
-	//FPGAFunctions f = DumpRead; //operation to be performed
+#if FPGADUMPREAD
+	FPGAFunctions f = DumpRead;  //operation to be performed
+#else
+	FPGAFunctions f = Read;      //operation to be performed
+#endif
 	// Compose the transmission FPGA header
 	MakeDeviceFrameTransmit(dev, b, d, f, (unsigned int)febNumber[0]);  
 
 	// the header + information part of the message 
+#if FPGADUMPREAD
+	OutgoingMessageLength = MinHeaderLength;              //length of the outgoing message message
+#else
 	OutgoingMessageLength = MinHeaderLength + NRegisters; //length of the outgoing message message
+#endif
 	TrueIncomingMessageLength = 
 		2 + MinHeaderLength + NRegisters + (NRegisters + 1) % 2; //the length of the incoming message
 	// Note above: incoming messages are ALWAYS 2 bytes LARGER than outgoing messages!
@@ -137,122 +138,37 @@ void feb::MakeMessage()
  * name inherited from Frames.  This function bit-packs the data into an OUTGOING
  * message from values set using the get/set functions assigned to this class (see feb.h).
  *
- * The packing for v83 firmware is described below.
+ * The packing for v83 firmware is described below. Header takes up First 11 bytes.  
+ * Registers start at indx==11.  See docdb 4311 for a description of the bit-by-bit packing.
+ * Note that we must clean up the outgoingMessages in the functions that call MakeMessage!
  ********************************************************************************
+ */
+#if FPGADUMPREAD
+	// Make a new out-going message buffer of suitable size.
+	outgoingMessage = new unsigned char [OutgoingMessageLength];  
+												
+	// Create a local "non-dyamic" (for lack of a better description)
+	// copy of the message buffer (dynamic) for use in working around
+	// a potential memory leak problems.  While mildly redundant, it 
+	// doesn't really cause any speed issues either and it helps 
+	// ensure that memory is cleaned up properly.
+	unsigned char localMessage[OutgoingMessageLength]; 
 
-Header takes up First 11 bytes.  Registers start at indx==11.
-
-FPGA[00]11 = Timer 1st byte (32 bits)
-FPGA[01]12 = Timer 2nd byte
-FPGA[02]13 = Timer 3rd byte
-FPGA[03]14 = Timer 4th byte
-FPGA[04]15 = Gate Start 1st byte (16 bits)
-FPGA[05]16 = Gate Start 2nd byte
-FPGA[06]17 = Gate Length 1st byte (16 bits)
-FPGA[07]18 = Gate Length 2nd byte
-FPGA[08]19 = DCM2 Phase Total 1st Byte (9 bits)
-FPGA[09]20 = 
-	0th bit-> DCM2 Phase Total 2nd Byte
-	1st bit-> DCM2 Phase Done (1 bit)
-	2nd bit-> DCM1 No Clock (1 bit)
-	3rd bit-> DCM2 No Clock (1 bit)
-	4th bit-> DCM1 Lock (1 bit)
-	5th bit-> DCM2 Lock (1 bit)
-	6th bit-> Test Pulse 2 Bit, Bit #0 (2 bit)
-	7th bit-> Test Pulse 2 Bit, Bit #1
-FPGA[10]21 = Phase Count (8 bits)
-FPGA[11]22 = 
-	0th bit-> Ext Trig Found (1 bit)
-	1st bit-> Ext Trig Rearm (1 bit)
-	2nd bit-> statusSCMDUnknown (1 bit) 
-	3rd bit-> statusFCMDUnknown (1 bit) 
-	4th bit-> Phase Increment  (1 bit)
-	5th bit-> Phase Start (1 bit)
-	6th bit-> statusRXLock (1 bit)      
-	7th bit-> statusTXSyncLock (1 bit)  
-FPGA[12]23 = Test Pulse Count 1st byte (32 bits)
-FPGA[13]24 = Test Pulse Count 2nd byte
-FPGA[14]25 = Test Pulse Count 3rd byte
-FPGA[15]26 = Test Pulse Count 4th byte
-FPGA[16]27 = Inject Count 0 (8 bits, enable included)
-FPGA[17]28 = Inject Count 1 (8 bits, enable included)
-FPGA[18]29 = Inject Count 2 (8 bits, enable included)
-FPGA[19]30 = Inject Count 3 (8 bits, enable included)
-FPGA[20]31 = Inject Count 4 (8 bits, enable included)
-FPGA[21]32 = Inject Count 5 (8 bits, enable included)
-FPGA[22]33 = 
-	0th bit-> Trip Power Off Bit#0 (6 bits)
-	1st bit-> Trip Power Off Bit#1
-	2nd bit-> Trip Power Off Bit#2
-	3rd bit-> Trip Power Off Bit#3
-	4th bit-> Trip Power Off Bit#4
-	5th bit-> Trip Power Off Bit#5
-	6th bit-> HV Manual (1 bit)
-	7th bit-> HV Enabled (1 bit)
-FPGA[23]34 = HV Target 1st byte (16 bits)
-FPGA[24]35 = HV Target 2nd byte 
-FPGA[25]36 = HV Actual 1st byte (16 bits)
-FPGA[26]37 = HV Actual 2nd byte
-FPGA[27]38 = HV Control (8 bits)
-FPGA[28]39 = Inject DAC Value 1st byte (12 bits)
-FPGA[29]40 = 
-	0th bit-> Inject DAC Value Bit#8
-	1st bit-> Inject DAC Value Bit#9
-	2nd bit-> Inject DAC Value Bit#10
-	3rd bit-> Inject DAC Value Bit#11
-	4th bit-> Inject DAC Mode Bit#0 (2 bits)
-	5th bit-> Inject DAC Mode Bit#1
-	6th bit-> Inject DAC Done (1 bit)
-	7th bit-> Inject DAC Start (1 bit)
-FPGA[30]41 = 
-	0th bit-> Inject Range Bit#0 (4 bits)
-	1st bit-> Inject Range Bit#1
-	2nd bit-> Inject Range Bit#2
-	3rd bit-> Inject Range Bit#3
-	4th bit-> Inject Phase Bit#0 (4 bits)
-	5th bit-> Inject Phase Bit#1
-	6th bit-> Inject Phase Bit#2
-	7th bit-> Inject Phase Bit#3
-FPGA[31]42 = 
-	0th bit-> Board ID Bit#0 (4 bits)
-	1st bit-> Board ID Bit#1
-	2nd bit-> Board ID Bit#2
-	3rd bit-> Board ID Bit#3
-	4th bit-> HV Num Avg Bit#0 (4 bits)
-	5th bit-> HV Num Avg Bit#1
-	6th bit-> HV Num Avg Bit#2
-	7th bit-> Spare Bit (Unused)
-FPGA[32]43 = Firmware Version (8 bits)
-FPGA[33]44 = HV Period Manual 1st byte (16 bits)
-FPGA[34]45 = HV Period Manual 2nd byte
-FPGA[35]46 = HV Period Auto 1st byte (16 bits)
-FPGA[36]47 = HV Period Auto 2nd byte
-FPGA[37]48 = HV Pulse Width (8 bits)
-FPGA[38]49 = Temperature 1st byte (16 bits)
-FPGA[39]50 = Temperature 2nd byte
-FPGA[40]51 = TripX Threshold (8 bits) 
-FPGA[41]52 = TRiP X Comp Enc (8 bits - really, 6 least significant bits (2 spare))  
-FPGA[42]53 = Discriminator Enable Mask TRiP 0 1st byte (16 bits)
-FPGA[43]54 = Discriminator Enable Mask TRiP 0 2nd byte
-FPGA[44]55 = Discriminator Enable Mask TRiP 1 1st byte (16 bits)
-FPGA[45]56 = Discriminator Enable Mask TRiP 1 2nd byte
-FPGA[46]57 = Discriminator Enable Mask TRiP 2 1st byte (16 bits)
-FPGA[47]58 = Discriminator Enable Mask TRiP 2 2nd byte
-FPGA[48]59 = Discriminator Enable Mask TRiP 3 1st byte (16 bits)
-FPGA[49]60 = Discriminator Enable Mask TRiP 3 2nd byte
-FPGA[50]61 = Gate Time Stamp 1st byte (32 bits)
-FPGA[51]62 = Gate Time Stamp 2nd byte
-FPGA[52]63 = Gate Time Stamp 3rd byte
-FPGA[53]64 = Gate Time Stamp 4th byte
-FPGA[54]65 = ??
-FPGA[55]66 = ??
-FPGA[56]67 = ??
---- Should end here
-
-*/
+	// Write the message to the localMessage buffer.
+	for (int i=0;i<(OutgoingMessageLength);i++) { 
+		if (i<MinHeaderLength) {
+			localMessage[i]=frameHeader[i];
+		} else {
+			localMessage[i]=message[i-MinHeaderLength];
+		}
+	}
+	// Put the message in the inherited out-going message bufer.
+	for (int i=0;i<(OutgoingMessageLength);i++) { 
+		outgoingMessage[i]=localMessage[i];
+	}
+#else
 	// Message must have an odd number of bytes!
 	message = new unsigned char [NRegisters + (NRegisters+1)%2]; 
-	// std::cout<<"NRegisters + (NRegisters+1)%2: "<<(NRegisters + (NRegisters+1)%2)<<std::endl;
 
 	/* message word 0 - 3:  The timer information, 32 bits for the timer */
 	message[0] = (Timer & 0xFF); //mask off bits 0-7
@@ -327,8 +243,7 @@ FPGA[56]67 = ??
 		The Injector counts 6 at 7 bits each, and the 8th bit of each word  is the enable status */
 	for (int i=16;i<22;i++ ) {
 		message[i] = InjectCount[(i-16)][0] & 0x7F; //mask off bits 0-6 (InjectCount)
-		message[i] |= (InjectEnable[(i-16)][0] & 0x01) << 0x07;  //mask off bit 0 (InjectEnable)
-														//and shift to bit 7
+		message[i] |= (InjectEnable[(i-16)][0] & 0x01) << 0x07; //mask off bit 0 (InjectEnable) and shift to bit 7
 	}
 
 	/* message word 22, bits 0-5:  trip power off, 1 bit for each trip 
@@ -393,7 +308,6 @@ FPGA[56]67 = ??
 	/* message word 41: TripXCompEnc, 6 (+2 spare) bits */
 	message[41] = (TripXCompEnc[0] & 0x3F); //mask off bits 0-5
 
-	// if (NRegisters==54)... // Firmware versions 78-84
 	/* message word 42-43 Discriminator Enable Mask Trip 0, 16 bits */
 	message[42] = (DiscrimEnableMask[0] & 0xFF); //mask off bits 0-7
 	message[43] = (DiscrimEnableMask[0] >> 0x08); // shift right 8 bits and mask is redundant here
@@ -440,6 +354,7 @@ FPGA[56]67 = ??
 	}
 	// Clean up memory.
 	delete [] message; 
+#endif // FPGADUMPREAD
 
 	// This finishes the outgoing message.
 }
@@ -733,39 +648,39 @@ void feb::SetFEBDefaultValues()
 	InjectRange[0] = 0;
 	InjectPhase[0] = 0;
 	InjectDACValue = 0;
-	InjectDACMode[0] = 0;
-	InjectDACDone[0] = 0;
+	InjectDACMode[0]  = 0;
+	InjectDACDone[0]  = 0;
 	InjectDACStart[0] = 0;
 	HVEnabled[0] = 0;
-	HVTarget = 32768;
-	HVActual = 0;
+	HVTarget     = 32768;
+	HVActual     = 0;
 	HVControl[0] = 0;
-	HVManual[0] = 0;
-	statusRXLock[0] = 0;     //actually readonly
-	statusTXSyncLock[0] = 0; //actually readonly
-	PhaseStart[0] = 0;
-	PhaseIncrement[0] = 0;
-	ExtTriggerFound[0] = 0; 
-	ExtTriggerRearm[0] = 0; 
+	HVManual[0]  = 0;
+	statusRXLock[0]      = 0;     //actually readonly
+	statusTXSyncLock[0]  = 0; //actually readonly
+	PhaseStart[0]        = 0;
+	PhaseIncrement[0]    = 0;
+	ExtTriggerFound[0]   = 0; 
+	ExtTriggerRearm[0]   = 0; 
 	statusSCMDUnknown[0] = 0; //actually readonly
 	statusFCMDUnknown[0] = 0; //actually readonly
 	PhaseCount[0] = 0;
-	DCM1Lock[0] = 0;
-	DCM2Lock[0] = 0;
+	DCM1Lock[0]   = 0;
+	DCM2Lock[0]   = 0;
 	DCM1NoClock[0] = 0;
 	DCM2NoClock[0] = 0;
-	DCM2PhaseDone[0] = 0;
-	DCM2PhaseTotal = 0;
-	TestPulse2Bit[0] = 0;
-	TestPulseCount = 0;
-	boardID[0] = 15;
+	DCM2PhaseDone[0]   = 0;
+	DCM2PhaseTotal     = 0;
+	TestPulse2Bit[0]   = 0;
+	TestPulseCount     = 0;
+	boardID[0]         = 15;
 	FirmwareVersion[0] = 0;
-	HVNumAve[0] = 0;
-	HVPeriodAuto = 0;
-	HVPeriodManual = 0;
+	HVNumAve[0]     = 0;
+	HVPeriodAuto    = 0;
+	HVPeriodManual  = 0;
 	HVPulseWidth[0] = 0;
-	Temperature = 0;
-	TripXThresh[0] = 0;
+	Temperature     = 0;
+	TripXThresh[0]  = 0;
 	TripXCompEnc[0] = 0;
 	for (int i=0; i<4; i++) {DiscrimEnableMask[i]=0xFFFF;} // default to discr. enabled
 	GateTimeStamp = 0; // readonly
