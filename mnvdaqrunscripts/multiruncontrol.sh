1b3c9a99c3602a6eb4d7e3055a652a1d5921355e
Gabriel Perdue <perdue@fnal.gov>
2010-09-13 13:04:00 -0500

    Update RC start scripts to first clear out any old clients.

--- mnvdaqrunscripts/multiruncontrol.sh
+++ mnvdaqrunscripts/multiruncontrol.sh
@@ -6,6 +6,15 @@ then
 	source $HOME/mnvdaqrunscripts/setupdaqenv.sh /work/software/mnvonline/mnvdaq
 fi
 
+# First, clear any old RC clients...
+$HOME/mnvdaqrunscripts/rckiller.pl
+
+# Now, start the RC
 pushd /work/software/mnvruncontrol/frontend
 python RunControl.py &
 popd
+echo "If you get a socket binding error, just close the RC and wait a minute and then try again."
+echo "If you get a warning about the last subrun not finishing cleanly, just wait for the status "
+echo "bar, and then click okay."
+
+
--- mnvdaqrunscripts/singleruncontrol.sh
+++ mnvdaqrunscripts/singleruncontrol.sh
@@ -6,6 +6,13 @@ then
 	source $HOME/mnvdaqrunscripts/setupdaqenv.sh /work/software/mnvsingle/mnvdaq
 fi
 
+# First, clear any old RC clients...
+$HOME/mnvdaqrunscripts/rckiller.pl
+
+# Now, start the RC
 pushd /work/software/mnvruncontrol/frontend
 python RunControl.py &
 popd
+echo "If you get a socket binding error, just close the RC and wait a minute and then try again."
+echo "If you get a warning about the last subrun not finishing cleanly, just wait for the status "
+echo "bar, and then click okay."
