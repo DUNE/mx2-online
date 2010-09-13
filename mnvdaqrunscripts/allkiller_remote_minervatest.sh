eaecc623856f507cbf58de3f7110912132205c72
Gabriel Perdue <perdue@fnal.gov>
2010-09-13 17:37:58 -0500

    Add script to kill remotely on the minervatest cluster.  Assumes a valid kerberos ticket.

--- mnvdaqrunscripts/allkiller_remote_minervatest.sh
+++ mnvdaqrunscripts/allkiller_remote_minervatest.sh
@@ -4,10 +4,6 @@ echo "Going to kill all ET processes..."
 $HOME/mnvdaqrunscripts/etkiller.pl
 echo "Waiting..."
 sleep 1
-echo "Going to kill all DAQ processes..."
-$HOME/mnvdaqrunscripts/daqkiller.pl
-echo "Waiting..."
-sleep 1
 echo "Going to kill the Run Control..."
 $HOME/mnvdaqrunscripts/rckiller.pl
 echo "Waiting..."
@@ -16,4 +12,9 @@ echo "Going to kill the Dispatcher..."
 $HOME/mnvdaqrunscripts/rdkiller.pl
 echo "Waiting..."
 sleep 1
+
+# Assumes a valid kerberos ticket!
+echo "Going to kill remote processes..."
+`ssh minerva@minervatest02.fnal.gov $HOME/mnvdaqrunscripts/allkiller.sh`
+`ssh minerva@minervatest04.fnal.gov $HOME/mnvdaqrunscripts/allkiller.sh`
 
