339d4e8f70175cb46f1df7eab5d6184cb506b9d7
Gabriel Perdue <perdue@fnal.gov>
2010-09-13 17:55:28 -0500

    Add the remote kill script for the mnvonline cluster too.

---
+++ mnvdaqrunscripts/allkiller_remote_mnvonline.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Going to kill all ET processes..."
+$HOME/mnvdaqrunscripts/etkiller.pl
+echo "Waiting..."
+sleep 1
+echo "Going to kill the Run Control..."
+$HOME/mnvdaqrunscripts/rckiller.pl
+echo "Waiting..."
+sleep 1
+echo "Going to kill the Dispatcher..."
+$HOME/mnvdaqrunscripts/rdkiller.pl
+echo "Waiting..."
+sleep 1
+
+# Assumes a valid kerberos ticket!
+echo "Going to kill remote processes..."
+`ssh mnvonline@mnvonline0.fnal.gov $HOME/mnvdaqrunscripts/allkiller_silent.sh`
+`ssh mnvonline@mnvonline1.fnal.gov $HOME/mnvdaqrunscripts/allkiller_silent.sh`
+
