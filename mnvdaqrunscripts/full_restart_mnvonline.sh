83288de0f6ad7c195b5ba21226fe4c659c6168eb
Gabriel Perdue <perdue@fnal.gov>
2010-09-14 09:57:16 -0500

    Add some output statements to tell shifters about the carnage they have unleashed...

--- mnvdaqrunscripts/full_restart_mnvonline.sh
+++ mnvdaqrunscripts/full_restart_mnvonline.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+echo "Logging onto mnvnearline1 and killing and restarting all nearline processes!"
 $HOME/mnvdaqrunscripts/remote_nearline_restart.sh
 sleep 1
+echo "Killing and restarting all DAQ processes on the mnvonline cluster!" 
 $HOME/mnvdaqrunscripts/allkiller_remote_mnvonline.sh
 
