fd1c6c5bbf583bf3883e7e491dc55feecc308cf1
Gabriel Perdue <perdue@fnal.gov>
2010-09-17 09:51:00 -0500

    Shouldn't use HOME for remote script when the value is different than local value.

--- mnvdaqrunscripts/nearline_restart.sh
+++ mnvdaqrunscripts/nearline_restart.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-$HOME/mnvdaqrunscripts/nearlinekiller.pl
-$HOME/mnvdaqrunscripts/start_om_dispatcher.sh
+#$HOME/mnvdaqrunscripts/nearlinekiller.pl
+#$HOME/mnvdaqrunscripts/start_om_dispatcher.sh
+/home/nearonline/mnvdaqrunscripts/nearlinekiller.pl
+/home/nearonline/mnvdaqrunscripts/start_om_dispatcher.sh
 
--- mnvdaqrunscripts/remote_nearline_restart.sh
+++ mnvdaqrunscripts/remote_nearline_restart.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-`ssh nearonline@mnvnearline1.fnal.gov source $HOME/mnvdaqrunscripts/nearline_restart.sh`
+#`ssh nearonline@mnvnearline1.fnal.gov source $HOME/mnvdaqrunscripts/nearline_restart.sh`
+`ssh nearonline@mnvnearline1.fnal.gov source /home/nearonline/mnvdaqrunscripts/nearline_restart.sh`
+
