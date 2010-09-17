6e165da84981797ed87b1cfe0744c6f8285937dc
Gabriel Perdue <perdue@fnal.gov>
2010-09-17 09:16:16 -0500

    Update to clean out gmplotters too.

--- mnvdaqrunscripts/chkdstkiller.pl
+++ mnvdaqrunscripts/chkdstkiller.pl
@@ -1,15 +1,17 @@
 #!/usr/bin/perl
 
-$proc = "python | grep mnvchkdst.py";
+$proc_list[0] = "python | grep mnvchkdst.py";
+$proc_list[1] = "gmplotter | grep gmplotter";
 
-while (!system("ps -fC $proc")) {
-	@fields = split/\s+/,`ps -fC $proc`;
-	$rcpid = $fields[1]; # 0 is user
-	print "Check DST PID is $rcpid... killing...\n";
-	system("kill -9 $rcpid");
-	print "Killed!\n";
-	print "Waiting...\n";
-	system("sleep 1");
+foreach $proc (@proc_list) {
+	while (!system("ps -fC $proc")) {
+		@fields = split/\s+/,`ps -fC $proc`;
+		$rcpid = $fields[1]; # 0 is user
+		print "Check DST PID is $rcpid... killing...\n";
+		system("kill -9 $rcpid");
+		print "Killed!\n";
+		print "Waiting...\n";
+		system("sleep 1");
+	}
 }
-
 
