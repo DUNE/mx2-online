104fa744ba7fa475fd114da7da8c22f5e882b365
cvs-fast-export <cvs-fast-export>
2010-09-27 18:05:37 +0000

    Synthetic commit for incomplete tag mnvonline_run00002595

--- mnvdaqrunscripts/nearline_restart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#$HOME/mnvdaqrunscripts/nearlinekiller.pl
-#$HOME/mnvdaqrunscripts/start_om_dispatcher.sh
-/home/nearonline/mnvdaqrunscripts/nearlinekiller.pl
-/home/nearonline/mnvdaqrunscripts/start_om_dispatcher.sh
-
