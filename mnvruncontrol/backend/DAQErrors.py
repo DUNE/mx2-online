93a539182c91486b556caab3761b21199818253c
Jeremy Wolcott <jwolcott@fnal.gov>
2010-11-02 17:08:29 -0400

    Finish docstring

--- mnvruncontrol/backend/DAQErrors.py
+++ mnvruncontrol/backend/DAQErrors.py
@@ -1,6 +1,11 @@
 """
-  Events.py:
-  Errors used by the run control.
+  DAQErrors.py:
+  Errors used by the DataAcquisitionManager.
+  
+  They are abstracted here so they can be properly
+  unpickled when sent via a PostOffice Message.
+  (It just works better when they're in their
+  own module.)
   
    Original author: J. Wolcott (jwolcott@fnal.gov)
                     Nov. 2010
