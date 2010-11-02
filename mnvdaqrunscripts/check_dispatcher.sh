1a6c0c2b63c4541f30afc39ebb299057100af30d
cvs-fast-export <cvs-fast-export>
2010-11-02 18:44:03 +0000

    Synthetic commit for incomplete tag v5r0p6

--- mnvdaqrunscripts/check_dispatcher.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#ps -leaf | grep dns.exe | grep -v grep
-ps -leaf | grep python | grep -v grep
-ps -leaf | grep MinervaNearline | grep -v grep
-ps -leaf | grep event_builder | grep -v grep
-
