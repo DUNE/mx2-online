82f9e91cf8394c80e46052c7af2073636ba60a73
cvs-fast-export <cvs-fast-export>
2010-11-03 14:05:30 +0000

    Synthetic commit for incomplete tag v5r1

--- mnvdaqrunscripts/remote_nearline_restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#`ssh nearonline@mnvnearline1.fnal.gov source $HOME/mnvdaqrunscripts/nearline_restart.sh`
-`ssh nearonline@mnvnearline1.fnal.gov source /home/nearonline/mnvdaqrunscripts/nearline_restart.sh`
-
