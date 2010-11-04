d342e1b7faf40329d1e0cd467c40364e4437db96
Howard Budd <hbudd@fnal.gov>
2010-11-04 14:33:26 -0500

    Fixing Gabe's typos.

--- mnvdaqrunscripts/remote_minervatest_dispatcher_restart_hard.sh
+++ mnvdaqrunscripts/remote_minervatest_dispatcher_restart_hard.sh
@@ -2,15 +2,15 @@
 
 # Assumes a valid kerberos ticket!
 echo "Going to kill remote processes..."
-`ssh minerva@minervatest2.fnal.gov $HOME/mnvdaqrunscripts/allkiller_silent.sh`
-`ssh minerva@minervatest4.fnal.gov $HOME/mnvdaqrunscripts/allkiller_silent.sh`
+`ssh minerva@minervatest02.fnal.gov $HOME/mnvdaqrunscripts/allkiller_silent.sh`
+`ssh minerva@minervatest04.fnal.gov $HOME/mnvdaqrunscripts/allkiller_silent.sh`
 echo "Waiting two seconds..."
 sleep 2
 
 # Assumes a valid kerberos ticket!
 echo "Now restart dispatchers..."
-`ssh minerva@minervatest2.fnal.gov source $HOME/mnvdaqrunscripts/multidispatcher.sh`
-`ssh minerva@minervatest4.fnal.gov source $HOME/mnvdaqrunscripts/multidispatcher.sh`
+`ssh minerva@minervatest02.fnal.gov source $HOME/mnvdaqrunscripts/multidispatcher.sh`
+`ssh minerva@minervatest04.fnal.gov source $HOME/mnvdaqrunscripts/multidispatcher.sh`
 echo "Waiting two seconds..."
 sleep 2
 
--- mnvdaqrunscripts/remote_minervatest_dispatcher_restart_soft.sh
+++ mnvdaqrunscripts/remote_minervatest_dispatcher_restart_soft.sh
@@ -2,8 +2,8 @@
 
 # Assumes a valid kerberos ticket!
 echo "Now restart dispatchers..."
-`ssh minerva@minervatest2.fnal.gov source $HOME/mnvdaqrunscripts/multidispatcher.sh`
-`ssh minerva@minervatest4.fnal.gov source $HOME/mnvdaqrunscripts/multidispatcher.sh`
+`ssh minerva@minervatest02.fnal.gov source $HOME/mnvdaqrunscripts/multidispatcher.sh`
+`ssh minerva@minervatest04.fnal.gov source $HOME/mnvdaqrunscripts/multidispatcher.sh`
 echo "Waiting two seconds..."
 sleep 2
 
