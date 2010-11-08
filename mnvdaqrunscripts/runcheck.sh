29ca6708c3739de0b43590387a8de5b5a5f6abb2
cvs-fast-export <cvs-fast-export>
2010-11-08 23:22:00 +0000

    Synthetic commit for incomplete tag gnp_2010_11_08a

--- mnvdaqrunscripts/runcheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ps -leaf | grep python | grep -v grep
-ps -leaf | grep event_builder | grep -v grep
-ps -leaf | grep et_monitor | grep -v grep
-ps -leaf | grep minervadaq | grep -v grep
-
