500b500cc01013f5a1ee610f7139c1cf497277ed
cvs-fast-export <cvs-fast-export>
2010-11-13 21:28:41 +0000

    Synthetic commit for incomplete tag v5r1p6

--- mnvdaqrunscripts/remote_minervatest_dispatcher_restart_soft.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Assumes a valid kerberos ticket!
-echo "Now restart dispatchers..."
-`ssh minerva@minervatest02.fnal.gov source /home/minerva/mnvdaqrunscripts/multidispatcher.sh`
-`ssh minerva@minervatest04.fnal.gov source /home/minerva/mnvdaqrunscripts/multidispatcher.sh`
-echo "Waiting two seconds..."
-sleep 2
-
