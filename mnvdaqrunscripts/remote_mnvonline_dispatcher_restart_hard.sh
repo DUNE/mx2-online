b179aaed506b36ceecf9f190c32ee01d7dc919bc
Gabriel Perdue <perdue@fnal.gov>
2010-11-22 17:36:09 -0600

    Minor fixes.

--- mnvdaqrunscripts/remote_mnvonline_dispatcher_restart_hard.sh
+++ mnvdaqrunscripts/remote_mnvonline_dispatcher_restart_hard.sh
@@ -5,16 +5,16 @@ MASTERMACH=mnvonlinemaster.fnal.gov
 SOLDERMACH=mnvonline0.fnal.gov
 WORKERMACH=mnvonline1.fnal.gov
 REMDAQACCT=mnvonline
-SCRIPTDIR=/home/${REMDAQACCT}
+SCRIPTDIR=/home/mnvonline/mnvdaqrunscripts
 
 # Assumes a valid kerberos ticket!
 echo "Going to kill remote processes..."
-`ssh ${REMDAQACCT}@${SOLDERMACH} ${SCRIPTDIR}/mnvdaqrunscripts/allkiller_silent.sh`
-`ssh ${REMDAQACCT}@${WORKERMACH} ${SCRIPTDIR}/mnvdaqrunscripts/allkiller_silent.sh`
+`ssh ${REMDAQACCT}@${SOLDERMACH} ${SCRIPTDIR}/allkiller_silent.sh`
+`ssh ${REMDAQACCT}@${WORKERMACH} ${SCRIPTDIR}/allkiller_silent.sh`
 sleep 2
 # Restart the dispatchers...
 echo "Now restarting the dispatchers..."
-`ssh ${REMDAQACCT}@${SOLDERMACH} source ${SCRIPTDIR}/mnvdaqrunscripts/multidispatcher.sh`
-`ssh ${REMDAQACCT}@${WORKERMACH} source ${SCRIPTDIR}/mnvdaqrunscripts/multidispatcher.sh`
+`ssh ${REMDAQACCT}@${SOLDERMACH} source ${SCRIPTDIR}/multidispatcher.sh`
+`ssh ${REMDAQACCT}@${WORKERMACH} source ${SCRIPTDIR}/multidispatcher.sh`
 sleep 2
 
