b4d834fbb2f84c92b2a58e4d327bee21fb4a8358
cvs-fast-export <cvs-fast-export>
2010-11-22 21:54:11 +0000

    Synthetic commit for incomplete tag jwolcott_2010_11_22

--- mnvdaqrunscripts/remote_mnvonline_dispatcher_restart_soft.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Assumes a valid kerberos ticket!
-echo "Now restart dispatchers..."
-`ssh mnvonline@mnvonline0.fnal.gov source /home/mnvonline/mnvdaqrunscripts/multidispatcher.sh`
-`ssh mnvonline@mnvonline1.fnal.gov source /home/mnvonline/mnvdaqrunscripts/multidispatcher.sh`
-echo "Waiting two seconds..."
-sleep 2
-
--- mnvdaqrunscripts/rsc26.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python2.6  /work/software/mnvruncontrol/frontend/RunSeriesConfigurator.py &
