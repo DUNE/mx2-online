3915df9df956cd368e1051397d76c72785d32de5
Gabriel Perdue <perdue@fnal.gov>
2010-11-22 15:21:38 -0600

    Directory clarification for minervatest, mnvonline clusters.

--- mnvdaqrunscripts/remote_mnvonline_dispatcher_restart_soft.sh
+++ mnvdaqrunscripts/remote_mnvonline_dispatcher_restart_soft.sh
@@ -2,8 +2,8 @@
 
 # Assumes a valid kerberos ticket!
 echo "Now restart dispatchers..."
-`ssh mnvonline@mnvonline0.fnal.gov source $HOME/mnvdaqrunscripts/multidispatcher.sh`
-`ssh mnvonline@mnvonline1.fnal.gov source $HOME/mnvdaqrunscripts/multidispatcher.sh`
+`ssh mnvonline@mnvonline0.fnal.gov source /home/mnvonline/mnvdaqrunscripts/multidispatcher.sh`
+`ssh mnvonline@mnvonline1.fnal.gov source /home/mnvonline/mnvdaqrunscripts/multidispatcher.sh`
 echo "Waiting two seconds..."
 sleep 2
 
