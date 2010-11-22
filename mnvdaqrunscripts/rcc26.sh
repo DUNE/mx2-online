7f551c7bd5c6bc80752ad0de0f9aca3e6dc5f720
cvs-fast-export <cvs-fast-export>
2010-11-22 23:44:28 +0000

    Synthetic commit for incomplete tag jwolcott_2010_11_22b

--- mnvdaqrunscripts/rcc26.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python2.6  /work/software/mnvruncontrol/frontend/RunControlConfiguration.py &
-
--- mnvdaqrunscripts/slowcontrol26.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-pushd /work/software/mnvconfigurator/SlowControl
-python2.6 SC_MainApp.py &
-popd
