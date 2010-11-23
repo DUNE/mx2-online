554aa98966b2527a0bbed783b398fc8d6d693b84
cvs-fast-export <cvs-fast-export>
2010-11-23 22:25:27 +0000

    Synthetic commit for incomplete tag jwolcott_2010_11_23

--- mnvdaqrunscripts/restart_all_nearline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Run this script while logged in to a nearonline machine to kill all 
-# monitoring processes and restart the dispatcher.
-
-/home/nearonline/mnvdaqrunscripts/nearlinekiller.pl
-/home/nearonline/mnvdaqrunscripts/start_om_dispatcher.sh
-
