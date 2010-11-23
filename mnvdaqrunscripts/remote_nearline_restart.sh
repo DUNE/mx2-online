77512caf9c195a1218e0890d92fd9ac1f6ffe904
cvs-fast-export <cvs-fast-export>
2010-11-23 01:34:39 +0000

    Synthetic commit for incomplete tag jwolcott_2010_11_22c

--- mnvdaqrunscripts/remote_nearline_restart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Run this script with valid kerberos authentication to restart the near online processes from a 
-# remote machine.
-
-`ssh nearonline@mnvnearline1.fnal.gov source /home/nearonline/mnvdaqrunscripts/restart_all_nearline.sh`
-
