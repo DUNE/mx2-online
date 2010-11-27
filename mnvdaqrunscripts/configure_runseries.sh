fdae7df227ffd15a12e13c429a503ed13eaace1d
Gabriel Perdue <perdue@fnal.gov>
2010-11-27 17:16:13 -0600

    Some misc cleanup.

--- mnvdaqrunscripts/configure_runseries.sh
+++ mnvdaqrunscripts/configure_runseries.sh
@@ -4,6 +4,5 @@
 
 which python2.6 >& /tmp/pytest.txt
 PYV=`perl -ne 'if (/no/) { print "python"; } else { print "python2.6"; }' /tmp/pytest.txt`
-echo "Using python version: $PYV"
 
 $PYV  ${RCROOT}/frontend/RunSeriesConfigurator.py &
--- mnvdaqrunscripts/whcr_configure_runcontrol.sh
+++ mnvdaqrunscripts/whcr_configure_runcontrol.sh
@@ -4,7 +4,6 @@
 
 which python2.6 >& /tmp/pytest.txt
 PYV=`perl -ne 'if (/no/) { print "python"; } else { print "python2.6"; }' /tmp/pytest.txt`
-echo "Using python version: $PYV"
 
 $PYV  ${RCROOT}/frontend/RunControlConfiguration.py &
 
