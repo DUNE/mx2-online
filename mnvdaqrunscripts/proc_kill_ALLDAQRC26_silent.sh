90732743b77c910206d2b5954ff2d6af2f714abc
Gabriel Perdue <perdue@fnal.gov>
2010-11-28 17:48:36 -0600

    Fix a typo.

--- mnvdaqrunscripts/proc_kill_ALLDAQRC26_silent.sh
+++ mnvdaqrunscripts/proc_kill_ALLDAQRC26_silent.sh
@@ -4,7 +4,7 @@ $HOME/mnvdaqrunscripts/proc_kill_ET.pl >& /dev/null
 sleep 1
 $HOME/mnvdaqrunscripts/proc_kill_mnvdaq.pl >& /dev/null
 sleep 1
-$HOME/mnvdaqrunscripts/proc_kill_RunCo.pl >& /dev/null
+$HOME/mnvdaqrunscripts/proc_kill_RunCo26.pl >& /dev/null
 sleep 1
 $HOME/mnvdaqrunscripts/proc_kill_ReadDisp.pl >& /dev/null
 sleep 1
