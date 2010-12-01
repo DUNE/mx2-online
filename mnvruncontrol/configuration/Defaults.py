1a48bd8eb6f927fc00cb9788e454414de466b257
cvs-fast-export <cvs-fast-export>
2010-12-01 22:47:19 +0000

    Synthetic commit for incomplete tag v3r1p3

--- mnvruncontrol/configuration/Defaults.py
+++ mnvruncontrol/configuration/Defaults.py
@@ -1,101 +1,60 @@
 """
    Defaults.py:
-    Default values for the various modules in the run control software.
-    They are centralized here for easier modification of the setup.
+   Default values for the various modules in the run control software.
+   They are centralized here for easier modification of the setup.
    
-    Original author: J. Wolcott (jwolcott@fnal.gov)
-                     Feb.-Apr. 2010
+   Original author: J. Wolcott (jwolcott@fnal.gov)
+                    Feb.-Mar. 2010
                     
-    Address all complaints to the management.
+   Address all complaints to the management.
 """
 
-# general stuff
-NOTIFY_ADDRESSES = ["jwolcott@fnal.gov",]
-
-CONFIG_DB_NAME = "run_control_config.db"
-CONFIG_DB_LOCATION = "/work/conditions"
-
-LOGFILE_GATE_COUNT_INTERVAL = 50	# the master log file will get a notice every this many gates
+import time
 
 # Data acquisition properties.
-EVENT_SIZE                = 2048 
-FRAMES                    = 4
-NUM_FEBS                  = 509				# full detector
-DAQ_HEADER_VERSION_STRING = "v09"
+EVENT_SIZE = 2048 
+FRAMES = 8
 
-# LI settings
-LI_ONE_PE_VOLTAGE = 5.07		# from Brandon Eberly (eberly@fnal.gov), 3/5/2010
-LI_MAX_PE_VOLTAGE = 12.07	# currently the maximum the LI box can output.
+# Run control properties.
+CONFIG_DB_LOCATION = "/work/conditions/run_control_config.db"
 
-# Front-end properties
-BLINK_INTERVAL                = 2		# in seconds
-BELL_INTERVAL                 = 10	# in seconds
-FRONTEND_MAX_TRIGGER_INTERVAL = 5  # in minutes
-FRONTEND_LISTEN_PORT          = 3000
-RESOURCE_LOCATION_DEFAULT     = "/work/software/mnvruncontrol/resources"
-
-#LOGFILE_NAME_PATTERN = "^(?P<detector>\w\w)_(?P<run>\d{8})_(?P<subrun>\d{4})_(?P<type>\w{5})_v\d+_(?P<year>\d{2})(?P<month>\d{2})(?P<day>\d{2})(?P<hour>\d{2})(?P<minute>\d{2})_Controller(?P<controller>[01])Log.txt$"
-
-# logging!
-FRONTEND_LOGFILE_DEFAULT    = "/tmp/runcontrol.log"
-LOGFILE_LOCATION_DEFAULT    = "/work/data/logs"
-OM_LOGFILE_LOCATION_DEFAULT = "/work/logs"
-OM_DISPATCHER_LOGFILE       = "%s/om_dispatcher.log" % OM_LOGFILE_LOCATION_DEFAULT
-PO_LOGFILE_DEFAULT          = "/work/data/logs/postoffice.log"
-RC_LOGFILE_DEFAULT          = "%s/runcontrol.log" % LOGFILE_LOCATION_DEFAULT
-
-# Socket communication defaults.
-DISPATCHER_PORT     = 1098
-ET_PORT_BASE        = 1201
-MASTER_PORT         = 1090
-NUM_ET_PORTS_TO_USE = 50
-
-CONNECTION_ATTEMPT_INTERVAL = 0.5	# in seconds
-MAX_CONNECTION_ATTEMPTS     = 3
-MESSAGE_TIMEOUT             = 5  # in seconds
-
-# various dispatcher file locations
-ET_SYSTEM_LOCATION_DEFAULT     = "/work/data/etsys"
-LAST_TRIGGER_FILE              = "/work/conditions/last_trigger.dat"
-MASTER_DISPATCHER_PIDFILE      = "/work/conditions/daqmgr_dispatcher.pid"
-MTEST_INSTALL_LOCATION         = "/home/nuhep/daq"
-MTEST_DATA_LOCATION            = "/home/nuhep/daq/DATA"
-MTEST_DISPATCHER_PIDFILE       = "/tmp/beamline_dispatcher.pid"
-MTEST_LOGFILE_LOCATION         = "/work/logs"
-MTEST_DISPATCHER_LOGFILE       = "%s/beamline_dispatcher.log" % MTEST_LOGFILE_LOCATION
-OM_DISPATCHER_PIDFILE          = "/tmp/om_dispatcher.pid"
-OM_GAUDI_OUTPUTOPTIONSFILE     = "/work/conditions/outputfilenames.opts" # use "/minerva/data/online_processing/swap_area/outputfilenames.opts" on mnvnearline1
-OM_GAUDI_INPUTOPTIONSFILE      = "/work/conditions/inputfilenames.opts"  #     "/minerva/data/online_processing/swap_area/inputfilenames.opts"
-OM_DATAFILE_LOCATION_DEFAULT   = "/work/data/rawdata"                    #     "/minerva/data/online_processing/swap_area"
-RAW_DATA_LOCATION_DEFAULT      = "/work/data/rawdata"
-READOUT_DISPATCHER_PIDFILE     = "/work/conditions/readout_dispatcher.pid"
-READOUT_DISPATCHER_LOGFILE     = "/work/data/logs/readout_dispatcher.log"
 RUN_SERIES_DB_LOCATION_DEFAULT = "/work/conditions/run_series"
 RUN_SUBRUN_DB_LOCATION_DEFAULT = "/work/conditions/next_run_subrun.db"
-SAM_FILE_LOCATION              = "/work/data/sam"
-SLOWCONTROL_CONFIG_LOCATION_DEFAULT  = "/work/conditions/MParamFiles/data/DAQ/hardware_config"
+LOGFILE_LOCATION_DEFAULT = "/work/data/logs"
+RC_LOGFILE_DEFAULT = LOGFILE_LOCATION_DEFAULT + "/runcontrol.log"
 
-# mtest configuration
-MTEST_BRANCH             = 1
-MTEST_CRATE              = 0
-MTEST_TYPE               = 1
-MTEST_MEM_SLOT           = 8
-MTEST_GATE_SLOT          = 18
-MTEST_ADC_SLOT           = 3
-MTEST_TDC_SLOT           = 2
-MTEST_TOF_RST_GATE_SLOT  = 18
-MTEST_PCOS_RST_GATE_SLOT = 20
+ET_SYSTEM_LOCATION_DEFAULT = "/work/data/etsys"
+RAW_DATA_LOCATION_DEFAULT = "/work/data/rawdata"
 
-# monitoring
-OM_DST_TARGET_PATH    = "/minerva/data/users/minerva/data_processing/dst"		# where DSTs are copied when they're done being created by the dispatcher
-OM_DST_MIN_JOB_TIME   = 10		# the minimum amount of time a DSTMaker job can take (anything shorter and mail is sent to NOTIFY_ADDRESSES)
-OM_CONDOR_HOST        = "mnvnearline1.fnal.gov"
-OM_MAX_CONDOR_BACKLOG = 2		# max number of jobs that can be backlogged in the Condor queue
+RESOURCE_LOCATION_DEFAULT = "/work/software/mnvruncontrol/resources"
+
+# Socket communication defaults.
+DISPATCHER_PORT = 1098
+MASTER_PORT     = 1090
+ET_PORT_BASE    = 1091
+
+SOLDIER = "mnvonline0.fnal.gov"
+WORKER  = "mnvonline1.fnal.gov"
+#MASTER  = "mnvonlinemaster.fnal.gov"
+MASTER = "localhost"
+
+MAX_CONNECTION_ATTEMPTS = 3
+CONNECTION_ATTEMPT_INTERVAL = 0.5	# in seconds
+SOCKET_TIMEOUT = 2.5  # in seconds
+
+# number of times in a row to log the same request
+# in the dispatcher before suppressing
+MAX_REPEATED_REQUEST_LOGS = 5
+
+# number of times in a row to log the same request
+# in the dispatcher before suppressing
+MAX_REPEATED_REQUEST_LOGS = 5
+
+# dispatcher process details
+DISPATCHER_PIDFILE = "/work/conditions/rc_dispatcher.pid"
+DISPATCHER_LOGFILE = "/work/data/logs/dispatcher.log"
 
 # slow control
-SLOWCONTROL_ALLOWED_HV_THRESHOLDS    = {200: 0}	# that is, { threshold : num allowed over that threshold, ... }
+SLOWCONTROL_CONFIG_LOCATION_DEFAULT = "/work/conditions/MParamFiles/data/DAQ/hardware_config"
+SLOWCONTROL_ALLOWED_HV_THRESHOLDS = {300: 0, 100: 3, 60: 15}	# that is, { threshold : num allowed over that threshold, ... }
 SLOWCONTROL_ALLOWED_PERIOD_THRESHOLD = 15000
-SLOWCONTROL_NUM_WRITE_ATTEMPTS       = 3
-SLOWCONTROL_BEAM_FILE                = "beam.hwcfg"
-SLOWCONTROL_LI_FILE                  = "li.hwcfg"
-SLOWCONTROL_LI_DISCRIMINATORS_FILE   = "li_with_discriminators.hwcfg"
