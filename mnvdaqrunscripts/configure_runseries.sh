f14442dc4bc1cf08b4c62030127b25db8307dbec
cvs-fast-export <cvs-fast-export>
2010-12-12 22:59:54 +0000

    Synthetic commit for incomplete tag v5r2p5

--- mnvdaqrunscripts/configure_runseries.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-. $HOME/mnvdaqrunscripts/defs_standardpaths
-
-which python2.6 >& /tmp/pytest.txt
-PYV=`perl -ne 'if (/no/) { print "python"; } else { print "python2.6"; }' /tmp/pytest.txt`
-
-$PYV  ${RCROOT}/frontend/RunSeriesConfigurator.py &
