26a4cff471590c0d751eb5f4e88e4362584b90cb
Gabriel Perdue <perdue@fnal.gov>
2010-12-13 10:30:04 -0600

    Make the soft links we need.

---
+++ mnvdaqrunscripts/whcr_setup_home_directory.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ln -sf /minerva/app/users/minerva/cmtuser/ /home/minerva/cmtuser
+ln -sf /minerva/data/online_processing/swap_area/ /home/minerva/dst_auto
+ln -sf /minerva/data/users/minerva/data_processing/dst_local/ /home/minerva/dst_local
+ln -sf /minerva/data/users/minerva/data_processing/event_displays/ /home/minerva/event_displays
+ln -sf /minerva/data/users/minerva/gmbrowser/plots/ /home/minerva/gmbrowser_plots
+ln -sf /minerva/data/users/minerva/gmbrowser/plotter/ /home/minerva/run_summary_plots
+
+
