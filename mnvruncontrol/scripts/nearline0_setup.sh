b044daae8b66837c0199d251b3df312d5f1f83a6
cvs-fast-export <cvs-fast-export>
2010-12-13 16:30:04 +0000

    Synthetic commit for incomplete tag v3r3p2

--- mnvruncontrol/scripts/nearline0_setup.sh
+++ mnvruncontrol/scripts/nearline0_setup.sh
@@ -1,14 +1,10 @@
 export LOCALE=NEARLINE
 export DIM_DNS_NODE=mnvnearline1.fnal.gov
+export UTGID=NEARONLINE
 
 source /work/mnvdaq/setupdaqenv.sh
 source /home/nearonline/cmtuser/setup.sh
-pushd /work/mnvsoft/v7r5/minerva/MINERVA/MINERVA_v7r5/DaqRecv/cmt/
-source ./setup.sh
-popd
+source /work/mnvsoft/v7r2/minerva/MINERVA/MINERVA_v7r2/DaqRecv/cmt/setup.sh
 
 export PATH=/work/python/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=/work:$PYTHONPATH
-export CORAL_AUTH_PATH=/home/nearonline/private
-export DIM_DNS_NODE=localhost
+
