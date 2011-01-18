ff4daa975901e7ebfc88e70fea1674ccf2936af6
Gabriel Perdue <perdue@fnal.gov>
2011-01-18 15:20:56 -0600

    v7r8 - officially sanction J. Wolcott's new exit codes.

--- mnvdaq/main/minervadaq.cpp
+++ mnvdaq/main/minervadaq.cpp
@@ -1866,7 +1866,7 @@ int WriteSAM(const char samfilename[],
 	fprintf(sam_file,"dataTier='binary-raw',\n");
 #endif
 	fprintf(sam_file,"runNumber=%d%04d,\n",runNum,subNum);
-	fprintf(sam_file,"applicationFamily=ApplicationFamily('online','v09','v07-07-10'),\n"); //online, DAQ Heder, CVSTag
+	fprintf(sam_file,"applicationFamily=ApplicationFamily('online','v09','v07-08-00'),\n"); //online, DAQ Heder, CVSTag
 	fprintf(sam_file,"fileSize=SamSize('0B'),\n");
 	fprintf(sam_file,"filePartition=1L,\n");
 	switch (detector) { // Enumerations set by the DAQHeader class.
