ac6999494018e408e1dae02434e12a5121f7ec14
Gabriel Perdue <perdue@fnal.gov>
2011-02-15 11:37:56 -0600

    v7r9: Bless J. Wolcott's latest changes.

--- mnvdaq/main/minervadaq.cpp
+++ mnvdaq/main/minervadaq.cpp
@@ -1868,7 +1868,7 @@ int WriteSAM(const char samfilename[],
 	fprintf(sam_file,"dataTier='binary-raw',\n");
 #endif
 	fprintf(sam_file,"runNumber=%d%04d,\n",runNum,subNum);
-	fprintf(sam_file,"applicationFamily=ApplicationFamily('online','v09','v07-08-00'),\n"); //online, DAQ Heder, CVSTag
+	fprintf(sam_file,"applicationFamily=ApplicationFamily('online','v09','v07-09-00'),\n"); //online, DAQ Heder, CVSTag
 	fprintf(sam_file,"fileSize=SamSize('0B'),\n");
 	fprintf(sam_file,"filePartition=1L,\n");
 	switch (detector) { // Enumerations set by the DAQHeader class.
