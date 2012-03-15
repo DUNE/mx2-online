c83cea4c9006773025bf86b62ba28c7c91d54f6d
Jeremy Wolcott <jwolcott@fnal.gov>
2012-03-15 12:54:16 -0400

    Keep a month's worth of log files, instead of a few days' worth.

--- mnvruncontrol/configuration/Logging.py
+++ mnvruncontrol/configuration/Logging.py
@@ -15,6 +15,13 @@ import logging.handlers
 
 from mnvruncontrol.configuration import Configuration
 
+# keep 30 days' worth of logs.
+ROTATING_LOG_COMMON_CONFIG = {
+	"when":        "midnight",
+	"backupCount": 30,
+	"delay":       True,
+}
+
 try:
 	alreadyImported
 except NameError:
@@ -38,28 +45,28 @@ except NameError:
 	# the online monitoring node
 	om_logger = logging.getLogger("Dispatcher.OM")
 	om_logger.setLevel(logging.INFO)
-	om_filehandler = logging.handlers.RotatingFileHandler(Configuration.params["log_OM"], maxBytes=204800, backupCount=5, delay=True)
+	om_filehandler = logging.handlers.TimedRotatingFileHandler(Configuration.params["log_OM"], **ROTATING_LOG_COMMON_CONFIG)
 	om_filehandler.setFormatter(formatter)
 	om_logger.addHandler(om_filehandler)
 
 	# readout node
 	readout_logger = logging.getLogger("Dispatcher.Readout")
 	readout_logger.setLevel(logging.DEBUG)
-	readout_filehandler = logging.handlers.RotatingFileHandler(Configuration.params["log_readout"], maxBytes=204800, backupCount=5, delay=True)
+	readout_filehandler = logging.handlers.TimedRotatingFileHandler(Configuration.params["log_readout"], **ROTATING_LOG_COMMON_CONFIG)
 	readout_filehandler.setFormatter(formatter)
 	readout_logger.addHandler(readout_filehandler)
 
 	# MTest beamline DAQ node
 	mtest_logger = logging.getLogger("Dispatcher.MTest")
 	mtest_logger.setLevel(logging.INFO)
-	mtest_filehandler = logging.handlers.RotatingFileHandler(Configuration.params["log_mtest"], maxBytes=204800, backupCount=5, delay=True)
+	mtest_filehandler = logging.handlers.TimedRotatingFileHandler(Configuration.params["log_mtest"], **ROTATING_LOG_COMMON_CONFIG)
 	mtest_filehandler.setFormatter(formatter)
 	mtest_logger.addHandler(mtest_filehandler)
 
 	# DAQMgr
 	daqmgr_logger = logging.getLogger("Dispatcher.DAQManager")
 	daqmgr_logger.setLevel(logging.DEBUG)
-	daqmgr_filehandler = logging.handlers.RotatingFileHandler(Configuration.params["log_master"], maxBytes=204800, backupCount=5, delay=True)
+	daqmgr_filehandler = logging.handlers.TimedRotatingFileHandler(Configuration.params["log_master"], **ROTATING_LOG_COMMON_CONFIG)
 	daqmgr_filehandler.setFormatter(formatter)
 	daqmgr_logger.addHandler(daqmgr_filehandler)
 
@@ -67,7 +74,7 @@ except NameError:
 	# the post office
 	PO_logger = logging.getLogger("PostOffice")
 
-	PO_filehandler = logging.handlers.RotatingFileHandler(Configuration.params["log_PO"], maxBytes=204800, backupCount=5, delay=True)
+	PO_filehandler = logging.handlers.TimedRotatingFileHandler(Configuration.params["log_PO"], **ROTATING_LOG_COMMON_CONFIG)
 	PO_logger.setLevel(logging.INFO)
 	PO_filehandler.setLevel(logging.INFO)
 	PO_filehandler.setFormatter(formatter)
@@ -84,7 +91,7 @@ except NameError:
 	# the frontend
 	frontend_logger = logging.getLogger("Frontend")
 	frontend_logger.setLevel(logging.DEBUG)
-	frontend_filehandler = logging.handlers.RotatingFileHandler(Configuration.params["log_frontend"], maxBytes=204800, backupCount=5, delay=True)
+	frontend_filehandler = logging.handlers.TimedRotatingFileHandler(Configuration.params["log_frontend"], **ROTATING_LOG_COMMON_CONFIG)
 	frontend_filehandler.setLevel(logging.DEBUG)
 	frontend_filehandler.setFormatter(formatter)
 	frontend_logger.addHandler(frontend_filehandler)
