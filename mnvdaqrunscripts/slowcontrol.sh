084f51d751044de110ec46309253b04bb8e2537e
cvs-fast-export <cvs-fast-export>
2012-03-19 18:51:07 +0000

    Synthetic commit for incomplete tag mnvonline_run00003829

--- mnvdaqrunscripts/slowcontrol.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-which python2.6 >& /tmp/pytest.txt
-PYV=`perl -ne 'if (/no/) { print "python"; } else { print "python2.6"; }' /tmp/pytest.txt`
-
-pushd /work/software/mnvconfigurator/SlowControl >& /dev/null
-$PYV SC_MainApp.py &
-popd >& /dev/null
