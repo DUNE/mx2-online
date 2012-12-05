8805ad9d0f01b34266c836d1c0ef54defa854400
Gabriel Perdue <perdue@fnal.gov>
2012-12-05 16:46:21 -0600

    Add a new ecroc class to represent the CROC-E proper. It compiles, but currently has some wrong behavior (incorrection register configuration messages, etc.).

---
+++ minervadaq/fermihardware/src/ecroc.cpp
@@ -0,0 +1,172 @@
+#ifndef ecroc_cpp
+#define ecroc_cpp
+
+#include "ecroc.h"
+
+/*********************************************************************************
+* Class for creating Chain Read-Out Controller objects for use with the 
+* MINERvA data acquisition system and associated software projects.
+*
+* Elaine Schulte, Rutgers University
+* Gabriel Perdue, The University of Rochester
+*
+**********************************************************************************/
+
+// log4cpp category hierarchy.
+log4cpp::Category& ecrocLog = log4cpp::Category::getInstance(std::string("ecroc"));
+
+//----------------------------------------
+ecroc::ecroc(unsigned int address, int ecrocid, 
+	CVAddressModifier modifier, CVDataWidth width, CVDataWidth swappedWidth,
+	log4cpp::Appender* appender ) 
+{
+	vmeAddress       = address;
+	id               = ecrocid;
+	addressModifier  = modifier; 
+	dataWidth        = width; 
+	dataWidthSwapped = swappedWidth;
+
+	timingSetupAddress           = address + ecrocTimingSetup;
+	resetAndTestPulseMaskAddress = address + ecrocResetAndTestPulseMask;
+	channelResetAddress          = address + ecrocChannelReset;
+	fastCommandAddress           = address + ecrocFastCommand;
+	testPulseAddress             = address + ecrocTestPulse;
+	rdfePulseDelayAddress        = address + ecrocRdfePulseDelay;
+	rdfePulseCommandAddress      = address + ecrocRdfePulseCommand;
+
+	SetupChannels(); 
+
+	ecrocAppender = appender;
+
+	InitializeRegisters((ecrocRegisters) 0x1, 0x0, 0x0 );
+}
+
+//----------------------------------------
+ecroc::~ecroc() 
+{ 
+	for (std::list<echannels*>::iterator p=ecrocChannels.begin();
+			p!=ecrocChannels.end();
+			p++) 
+		delete (*p);
+	ecrocChannels.clear();
+}
+
+//----------------------------------------
+unsigned int ecroc::GetAddress() 
+{
+	return vmeAddress;
+}
+
+//----------------------------------------
+int ecroc::GetCrocID() 
+{
+	return id;
+}
+
+//----------------------------------------
+CVAddressModifier ecroc::GetAddressModifier() 
+{
+	return addressModifier;
+}
+
+//----------------------------------------
+CVDataWidth ecroc::GetDataWidth() 
+{
+	return dataWidth;
+}
+
+//----------------------------------------
+CVDataWidth ecroc::GetDataWidthSwapped() 
+{
+	return dataWidthSwapped;
+}
+
+//----------------------------------------
+void ecroc::SetupChannels() 
+{
+	for ( unsigned int i=0; i<4; ++i ) { 
+		echannels *tmp = new echannels( vmeAddress, i );
+		ecrocChannels.push_back( tmp ); 
+	}
+}
+
+//----------------------------------------
+echannels* ecroc::GetChannel( unsigned int i ) { 
+	echannels *tmp=0; 
+	for (std::list<echannels*>::iterator p=ecrocChannels.begin(); 
+			p!=ecrocChannels.end(); 
+			++p) { 
+		if ( i == (*p)->GetChannelNumber() ) tmp = (*p); 
+	}
+	return tmp; 
+}
+
+//----------------------------------------
+std::list<echannels*>* ecroc::GetChannelsList() 
+{
+	return &ecrocChannels;
+}
+
+//----------------------------------------
+unsigned int ecroc::GetTimingSetupAddress()
+{
+	return timingSetupAddress;
+}
+
+//----------------------------------------
+unsigned short ecroc::GetTimingRegisterMessage() 
+{
+	return timingRegisterMessage;
+}
+
+//----------------------------------------
+void ecroc::SetTimingRegisterMessage( unsigned short clockMode, 
+	unsigned short testPulseDelayEnabled, 
+	unsigned short testPulseDelayValue )
+{
+	timingRegisterMessage  = (clockMode & 0x1)<<15 ;            // the clock mode  (0x8000 is the bitmask for bit 15 high)
+	timingRegisterMessage |= (testPulseDelayEnabled & 0x1)<<12; // test pulse delay enable bit (bit 12)
+	timingRegisterMessage |= testPulseDelayValue & 0x3FF;       // test pules delay values (in 18.9 ns units) bits 0-9
+	timingRegisterMessage &= 0xFFFF;
+	ecrocLog.debugStream() << "    Timing Register = " << timingRegisterMessage; // TODO: hex!
+}
+
+//----------------------------------------
+void ecroc::SetResetAndTestPulseRegisterMessage( unsigned short resetEnable, unsigned short testPulseEnable )
+{
+	resetAndTestPulseMaskRegisterMessage  = resetEnable     & 0xF00; //the reset enable (bits 8-11)
+	resetAndTestPulseMaskRegisterMessage |= testPulseEnable & 0x00F; //the test pules enable (bits 0-3)
+}
+
+//----------------------------------------
+unsigned int ecroc::GetFastCommandAddress()
+{
+	return fastCommandAddress;
+}
+
+//----------------------------------------
+unsigned short ecroc::GetFastCommandRegisterMessage()
+{
+	return fastCommandRegisterMessage;
+}
+
+//----------------------------------------
+void ecroc::SetFastCommandRegisterMessage( unsigned short value ) 
+{
+	fastCommandRegisterMessage = value & 0xFF;
+}
+
+//----------------------------------------
+void ecroc::InitializeRegisters( ecrocRegisters clockMode, 
+		unsigned short testPulseDelayValue,
+		unsigned short testPulseDelayEnabled ) 
+{
+	SetTimingRegisterMessage( clockMode, testPulseDelayEnabled, testPulseDelayValue );
+	SetResetAndTestPulseRegisterMessage( 0, 0 );
+}
+
+
+
+
+
+#endif
