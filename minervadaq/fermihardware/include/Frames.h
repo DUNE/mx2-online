739f335278729056abf9c6f7df395786801cce94
Gabriel Perdue <perdue@fnal.gov>
2013-01-11 15:42:41 -0600

    Add some functions for reading the TRiPs.

--- minervadaq/fermihardware/include/Frames.h
+++ minervadaq/fermihardware/include/Frames.h
@@ -5,10 +5,7 @@
 #include <iostream>
 #include <fstream>
 
-/* CAEN VME headers go here */
-
 /* custom headers go here */
-
 #include "FrameTypes.h"
 #include "MinervaDAQtypes.h"
 #include "TripTTypes.h"
@@ -16,62 +13,61 @@
 #include "log4cppHeaders.h"
 
 /*********************************************************************************
-* Class for creating general Frame header objects for use with the 
-* MINERvA data acquisition system and associated software projects.
-*
-* Elaine Schulte, Rutgers University
-* Gabriel Perdue, The University of Rochester
-*
-**********************************************************************************/
+ * Class for creating general Frame header objects for use with the 
+ * MINERvA data acquisition system and associated software projects.
+ *
+ * Elaine Schulte, Rutgers University
+ * Gabriel Perdue, The University of Rochester
+ **********************************************************************************/
 
 /*! \class Frames
-*
-* \brief The base class for all LVDS data exchange frames.
-*
-*/
+ *
+ * \brief The base class for all LVDS data exchange frames.
+ *
+ */
 
 class Frames {
-/*! This class makes up the general base frames for use in communicating
- *  through the crate & cards.  
- */
-	protected:
-		static const int MinHeaderLength;
-		static const int ADCFrameLength;
-		static const int NDiscrChPerTrip;
-		unsigned char FrameID[2];        // Starts out Empty, filled on return of message
-		unsigned char frameHeader[9];    // a frame header, add this to the message
-		unsigned char *outgoingMessage;  // the character string for the message sent to a device
-		unsigned char broadcastCommand[1], febNumber[1], 
-			messageDirection[1], targetDevice[1], 
-			deviceFunction[1];
-		int IncomingMessageLength, OutgoingMessageLength;
+  /*! This class makes up the general base frames for use in communicating
+   *  through the crate & cards.  
+   */
+  protected:
+    static const int MinHeaderLength;
+    static const int ADCFrameLength;
+    static const int NDiscrChPerTrip;
+    unsigned char FrameID[2];        // Starts out Empty, filled on return of message
+    unsigned char frameHeader[9];    // a frame header, add this to the message
+    unsigned char *outgoingMessage;  // the character string for the message sent to a device
+    unsigned char broadcastCommand[1], febNumber[1], 
+                  messageDirection[1], targetDevice[1], 
+                  deviceFunction[1];
+    int IncomingMessageLength, OutgoingMessageLength;
 
-		// log4cpp appender for printing log statements.
-                log4cpp::Appender* frmsAppender;
-	
-	public:
-		Frames(log4cpp::Appender* appender);
-		virtual ~Frames() { };
+    // log4cpp appender for printing log statements.
+    log4cpp::Appender* frmsAppender;
 
-		unsigned char *message; //the message that will be sent or received. mostly received...
-		void printMessageBufferToLog( int buffersize );
+  public:
+    Frames(log4cpp::Appender* appender);
+    virtual ~Frames() { };
 
-		/*! transmission frame...These are for sending messages out */
-		void MakeDeviceFrameTransmit(Devices, Broadcasts, Directions, unsigned int, unsigned int); 
-		void MakeHeader();
-		virtual void MakeMessage();
-		virtual int DecodeRegisterValues(int a) = 0;
-		void DecodeHeader();
-		bool CheckForErrors();
+    unsigned char *message; //the message that will be sent or received. mostly received...
+    void printMessageBufferToLog( int buffersize );
 
-		inline unsigned char *GetOutgoingMessage() {return outgoingMessage;};
-		void DeleteOutgoingMessage() {delete [] outgoingMessage;};
+    /*! transmission frame...These are for sending messages out */
+    void MakeDeviceFrameTransmit(Devices, Broadcasts, Directions, unsigned int, unsigned int); 
+    void MakeHeader();
+    virtual void MakeMessage();
+    virtual int DecodeRegisterValues(int a) = 0;
+    void DecodeHeader();
+    bool CheckForErrors();
 
-		void inline SetIncomingMessageLength(int a) {IncomingMessageLength = a;};
-		int inline GetIncomingMessageLength() {return IncomingMessageLength;};
-		int inline GetOutgoingMessageLength() {return OutgoingMessageLength;};
-		int inline GetFEBNumber() {return (int)febNumber[0];};
-		int inline GetDeviceType() {return (int) targetDevice[0];};
+    inline unsigned char *GetOutgoingMessage() {return outgoingMessage;};
+    void DeleteOutgoingMessage() {delete [] outgoingMessage;};
+
+    void inline SetIncomingMessageLength(int a) {IncomingMessageLength = a;};
+    int inline GetIncomingMessageLength() {return IncomingMessageLength;};
+    int inline GetOutgoingMessageLength() {return OutgoingMessageLength;};
+    int inline GetFEBNumber() {return (int)febNumber[0];};
+    int inline GetDeviceType() {return (int) targetDevice[0];};
 
 };
 
