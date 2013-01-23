ab4d24a7307c84fc7c1c6beba00bd92113f4ca39
Gabriel Perdue <perdue@fnal.gov>
2013-01-23 14:23:36 -0600

    Start CRIM initialization implementation.

--- minervadaq/fermihardware/include/CRIM.h
+++ minervadaq/fermihardware/include/CRIM.h
@@ -14,11 +14,11 @@
 
 
 /*********************************************************************************
-* Class for creating Chain Read-Out Controller Interfact Module objects for 
-* use with the MINERvA data acquisition system and associated software projects.
-*
-* Gabriel Perdue, The University of Rochester
-**********************************************************************************/
+ * Class for creating Chain Read-Out Controller Interfact Module objects for 
+ * use with the MINERvA data acquisition system and associated software projects.
+ *
+ * Gabriel Perdue, The University of Rochester
+ **********************************************************************************/
 
 /*! \class CRIM
  *
@@ -30,198 +30,202 @@
 
 class CRIM : public VMECommunicator {
 
-	private:
+  private:
     /* We will always use the first CRIM in the vector of CRIMs held by the 
        DAQ worker classes as the master CRIM. */
-		log4cpp::Appender* CRIMAppender;
+    log4cpp::Appender* CRIMAppender;
 
     CVIRQLevels irqLevel;                /*!<the interrupt priority level */
-		CRIMInterrupts irqLine;              /*!<the interrupt to be monitored */
-		unsigned short interruptValue;       /*!<the bitmask for the interrupt line */
-		unsigned short interruptStatus;      /*!<the status of the interrupts */
-		unsigned short resetInterrupts;      /*!<the value to be sent to the clear interrupts register on the CRIM */
-		unsigned short interruptConfigValue; /*!<the configuration value sent to the
-		configuration register (must match the 
-		IRQLevel for the CAEN interrupt handler:
-		i.e. if this value is 5 (the default)
-		the IRQLevel for the CAEN must be IRQ5. */
+    CRIMInterrupts irqLine;              /*!<the interrupt to be monitored */
+    unsigned short interruptValue;       /*!<the bitmask for the interrupt line */
+    unsigned short interruptStatus;      /*!<the status of the interrupts */
+    unsigned short resetInterrupts;      /*!<the value to be sent to the clear interrupts register on the CRIM */
+    unsigned short interruptConfigValue; /*!<the configuration value sent to the
+                                           configuration register (must match the 
+                                           IRQLevel for the CAEN interrupt handler:
+                                           i.e. if this value is 5 (the default)
+                                           the IRQLevel for the CAEN must be IRQ5. */
 
-		unsigned short controlRegister; 
+    unsigned short controlRegister; 
 
-		/*! interrupt register addresses
-		* interruptAddress:  the address of the Interrupt Mask Register, i.e. which line to monitor
-		* interruptStatusRegister:  the address of the register which reports the interrupt status
-		* interruptsClear: the address to which a "clear" is sent to acknowledge pending interrupts
-		* interruptConfig:  the address to the register which sets the interrupt priority levels
-		*/
-		unsigned int interruptAddress, interruptStatusRegister, interruptsClear, interruptConfig;
+    /*! interrupt register addresses
+     * interruptAddress:  the address of the Interrupt Mask Register, i.e. which line to monitor
+     * interruptStatusRegister:  the address of the register which reports the interrupt status
+     * interruptsClear: the address to which a "clear" is sent to acknowledge pending interrupts
+     * interruptConfig:  the address to the register which sets the interrupt priority levels
+     */
+    unsigned int interruptAddress, interruptStatusRegister, interruptsClear, interruptConfig;
 
-		/*! timing & software register addresses */
-		unsigned int timingRegister, SGATEWidthRegister, TCALBDelayRegister, softwareTriggerRegister, 
-			softwareTCALBRegister, softwareSGATERegister, softwareCNRSTRegister;
+    /*! timing & software register addresses */
+    unsigned int timingRegister, SGATEWidthRegister, TCALBDelayRegister, softwareTriggerRegister, 
+                 softwareTCALBRegister, softwareSGATERegister, softwareCNRSTRegister;
 
-		/*!control & status register addresses */
-		unsigned int controlRegisterAddress;
-		unsigned int statusRegisterAddress;
-		unsigned int clearStatusRegister;
+    /*!control & status register addresses */
+    unsigned int controlRegisterAddress;
+    unsigned int statusRegisterAddress;
+    unsigned int clearStatusRegister;
 
-		/*! "external" data register addresses */
-		unsigned int gateTimeWordLowAddress;
-		unsigned int gateTimeWordHighAddress;
+    /*! "external" data register addresses */
+    unsigned int gateTimeWordLowAddress;
+    unsigned int gateTimeWordHighAddress;
 
-		/*! variables for holding information about the setup of the CRIM for use. */
-		unsigned short timingSetup, gateWidthSetup, TCALBDelaySetup;
-	
-		/*! cosmic mode control registers. */
-		unsigned int sequencerResetRegister;
-	
-		/*!  these are the various masks that are used to set up  running conditions */
-		static unsigned short const TimingSetupRegisterModeMask;
-		static unsigned short const TimingSetupRegisterFrequencyMask;
-		static unsigned short const GateWidthRegisterMask;
-		static unsigned short const GateWidthRegisterEnableCNRSTMask;
-		static unsigned short const TCALBDelayRegisterMask;
-		static unsigned short const InterruptMaskRegisterMask;
-		static unsigned short const InterruptStatusRegisterMask;
-		static unsigned short const ClearInterruptsRegisterMask;
-		static unsigned short const InterruptConfigRegisterIRQMask;
-		static unsigned short const InterruptConfigGlobalEnableMask;
-		static unsigned short const TriggerRegisterPulseMask;
-		static unsigned short const TCALBRegisterPulseMask;
-		static unsigned short const SGATERegisterStartMask;
-		static unsigned short const SGATERegisterStopMask;
-		static unsigned short const CNRSTRegisterPulseMask;
-		static unsigned short const CNRSTRegisterOneShotMask;
-		static unsigned short const ControlRegisterFETriggerMask;
-		static unsigned short const ControlRegisterCRCMask;
-		static unsigned short const ControlRegisterSendMask;
-		static unsigned short const ControlRegisterRetransmitMask;
-		static unsigned short const TimingCommandRegisterMask;
-		static unsigned short const SequencerControlLatchResetMask; // Must write to re-enable ext. trigger
-		static unsigned short const TimingViolationRegisterMask;
-		static unsigned short const TimingViolationRegisterClearMask; // write to clear timing violations reg.
-		static unsigned short const MinosSGATELowerBitsMask;
-		static unsigned short const MinosSGATEUpperBitsMask;
+    /*! variables for holding information about the setup of the CRIM for use. */
+    unsigned short timingSetup, gateWidthSetup, TCALBDelaySetup;
 
-		/*! execution values */
-		static const unsigned short softTrigger, softTCALB, softSGATEstart, 
-			softSGATEstop, softCNRST, softCNRSTseq;
+    /*! cosmic mode control registers. */
+    unsigned int sequencerResetRegister;
 
-	public:
-    
-		CRIM( unsigned int address, log4cpp::Appender* appender, Controller* controller ); 
-		~CRIM() { }; 
+    /*!  these are the various masks that are used to set up  running conditions */
+    static unsigned short const TimingSetupRegisterModeMask;
+    static unsigned short const TimingSetupRegisterFrequencyMask;
+    static unsigned short const GateWidthRegisterMask;
+    static unsigned short const GateWidthRegisterEnableCNRSTMask;
+    static unsigned short const TCALBDelayRegisterMask;
+    static unsigned short const InterruptMaskRegisterMask;
+    static unsigned short const InterruptStatusRegisterMask;
+    static unsigned short const ClearInterruptsRegisterMask;
+    static unsigned short const InterruptConfigRegisterIRQMask;
+    static unsigned short const InterruptConfigGlobalEnableMask;
+    static unsigned short const TriggerRegisterPulseMask;
+    static unsigned short const TCALBRegisterPulseMask;
+    static unsigned short const SGATERegisterStartMask;
+    static unsigned short const SGATERegisterStopMask;
+    static unsigned short const CNRSTRegisterPulseMask;
+    static unsigned short const CNRSTRegisterOneShotMask;
+    static unsigned short const ControlRegisterFETriggerMask;
+    static unsigned short const ControlRegisterCRCMask;
+    static unsigned short const ControlRegisterSendMask;
+    static unsigned short const ControlRegisterRetransmitMask;
+    static unsigned short const TimingCommandRegisterMask;
+    static unsigned short const SequencerControlLatchResetMask; // Must write to re-enable ext. trigger
+    static unsigned short const TimingViolationRegisterMask;
+    static unsigned short const TimingViolationRegisterClearMask; // write to clear timing violations reg.
+    static unsigned short const MinosSGATELowerBitsMask;
+    static unsigned short const MinosSGATEUpperBitsMask;
+
+    /*! execution values */
+    static const unsigned short softTrigger, softTCALB, softSGATEstart, 
+                 softSGATEstop, softCNRST, softCNRSTseq;
+
+    void logRunningMode( RunningModes runningMode );
+
+  public:
+
+    CRIM( unsigned int address, log4cpp::Appender* appender, Controller* controller ); 
+    ~CRIM() { }; 
 
     void Initialize( RunningModes runningMode );
 
-		unsigned int GetAddress();
+    unsigned int GetAddress();
     unsigned short GetStatus();
 
     // TODO : write to physical registers!
-		void SetupTiming( CRIMTimingModes a, CRIMTimingFrequencies b ); 
-		void SetupGateWidth( unsigned short tcalbEnable, unsigned short gateWidth); 
-		void SetupGateWidth( unsigned short tcalbEnable, unsigned short gateWidth, unsigned short sequencerEnable ); 
-		void SetupTCALBPulse( unsigned short pulseDelay );
+    void SetupTiming( CRIMTimingModes a, CRIMTimingFrequencies b ); 
+    void SetupGateWidth( unsigned short tcalbEnable, unsigned short gateWidth); 
+    void SetupGateWidth( unsigned short tcalbEnable, unsigned short gateWidth, unsigned short sequencerEnable ); 
+    void SetupTCALBPulse( unsigned short pulseDelay );
 
-		// functions which return the timing/trigger info
-		unsigned short GetGateWidthSetup() {return gateWidthSetup;};
-		unsigned short GetTCALBPulse() {return TCALBDelaySetup;};
-		unsigned short GetTimingSetup() {return timingSetup;};
-		unsigned short GetGateWidth() {return (gateWidthSetup & GateWidthRegisterMask);};
-		unsigned short GetGateTCALBEnable() {return ((gateWidthSetup & 0x8000)>>15);};
-		unsigned short GetTiming() {return ((timingSetup & TimingSetupRegisterModeMask)>>12);}; 
-		unsigned short GetSoftTrig() {return softTrigger;};
-		unsigned short GetSoftTCALB() {return softTCALB;};
-		unsigned short GetSoftSGATEStart() {return softSGATEstart;};
-		unsigned short GetSoftSGATEStop() {return softSGATEstop;};
-		unsigned short GetSoftCNRST() {return softCNRST;};
-		unsigned short GetSoftCNRSTSeq() {return softCNRSTseq;};
+    // functions which return the timing/trigger info
+    /*
+    unsigned short GetGateWidthSetup() {return gateWidthSetup;};
+    unsigned short GetTCALBPulse() {return TCALBDelaySetup;};
+    unsigned short GetTimingSetup() {return timingSetup;};
+    unsigned short GetGateWidth() {return (gateWidthSetup & GateWidthRegisterMask);};
+    unsigned short GetGateTCALBEnable() {return ((gateWidthSetup & 0x8000)>>15);};
+    unsigned short GetTiming() {return ((timingSetup & TimingSetupRegisterModeMask)>>12);}; 
+    unsigned short GetSoftTrig() {return softTrigger;};
+    unsigned short GetSoftTCALB() {return softTCALB;};
+    unsigned short GetSoftSGATEStart() {return softSGATEstart;};
+    unsigned short GetSoftSGATEStop() {return softSGATEstop;};
+    unsigned short GetSoftCNRST() {return softCNRST;};
+    unsigned short GetSoftCNRSTSeq() {return softCNRSTseq;};
 
-		unsigned int GetTimingRegister() {return timingRegister;};
-		unsigned int GetSGATEWidthRegister() {return SGATEWidthRegister;};
-		unsigned int GetTCALBRegister() {return TCALBDelayRegister;};
-		unsigned int GetSoftTriggerRegister() {return softwareTriggerRegister;};
-		unsigned int GetSoftTCALBRegister() {return softwareTCALBRegister;};
-		unsigned int GetSoftSGATERegister() {return softwareSGATERegister;};
-		unsigned int GetSoftCNRSTRegister() {return softwareCNRSTRegister;};
+    unsigned int GetTimingRegister() {return timingRegister;};
+    unsigned int GetSGATEWidthRegister() {return SGATEWidthRegister;};
+    unsigned int GetTCALBRegister() {return TCALBDelayRegister;};
+    unsigned int GetSoftTriggerRegister() {return softwareTriggerRegister;};
+    unsigned int GetSoftTCALBRegister() {return softwareTCALBRegister;};
+    unsigned int GetSoftSGATERegister() {return softwareSGATERegister;};
+    unsigned int GetSoftCNRSTRegister() {return softwareCNRSTRegister;};
+    */
 
-		/*! interrupt stuff */
-		void inline SetIRQLevel(CVIRQLevels a) {irqLevel = a;};  //sets the IRQ Level (CAEN)
-		void inline SetIRQLine(CRIMInterrupts a) {irqLine = a;}; //sets the IRQ Level (CAEN)
-		CVIRQLevels inline GetIRQLevel() {return irqLevel;}; //returns the irq level (CAEN)
-		unsigned char inline GetIRQLine() { return (unsigned char)irqLine; };
+    /*! interrupt stuff */
+    void inline SetIRQLevel(CVIRQLevels a) {irqLevel = a;};  //sets the IRQ Level (CAEN)
+    void inline SetIRQLine(CRIMInterrupts a) {irqLine = a;}; //sets the IRQ Level (CAEN)
+    CVIRQLevels inline GetIRQLevel() {return irqLevel;}; //returns the irq level (CAEN)
+    unsigned char inline GetIRQLine() { return (unsigned char)irqLine; };
 
-		void inline SetInterruptMask() {
-			interruptValue = ((unsigned short)irqLine & InterruptMaskRegisterMask);
-		}; 
+    void inline SetInterruptMask() {
+      interruptValue = ((unsigned short)irqLine & InterruptMaskRegisterMask);
+    }; 
 
-		void inline SetInterruptStatus(unsigned short a) {
-			interruptStatus = a & InterruptStatusRegisterMask;
-		}; 
+    void inline SetInterruptStatus(unsigned short a) {
+      interruptStatus = a & InterruptStatusRegisterMask;
+    }; 
 
-		void inline SetInterruptConfigValue(unsigned short a) {
-			interruptConfigValue = a;
-		}; 
-		
-		void inline SetInterruptGlobalEnable(bool a) {
-			interruptConfigValue |= ((a << 7) & InterruptConfigGlobalEnableMask);
-		}; 
+    void inline SetInterruptConfigValue(unsigned short a) {
+      interruptConfigValue = a;
+    }; 
 
-		unsigned short inline GetInterruptMask() {
-			return interruptValue;
-		}; 
-		
-		unsigned int inline GetInterruptMaskAddress() {
-			return interruptAddress;
-		}; 
-		
-		unsigned short inline GetInterruptStatus() {
-			return (interruptStatus & InterruptStatusRegisterMask);
-		}; 
+    void inline SetInterruptGlobalEnable(bool a) {
+      interruptConfigValue |= ((a << 7) & InterruptConfigGlobalEnableMask);
+    }; 
 
-		unsigned short inline GetInterruptGlobalEnable() {
-			return interruptConfigValue & InterruptConfigGlobalEnableMask;
-		}; 
-		
-		unsigned short inline GetInterruptConfig() {
-			return interruptConfigValue;
-		}; 
-		
-		unsigned int inline GetInterruptsConfigAddress() {
-			return interruptConfig;
-		}; 
+    unsigned short inline GetInterruptMask() {
+      return interruptValue;
+    }; 
 
-		unsigned int inline GetInterruptStatusAddress() {
-			return interruptStatusRegister;
-		}; 
+    unsigned int inline GetInterruptMaskAddress() {
+      return interruptAddress;
+    }; 
 
-		unsigned short inline GetClearInterrupts() {
-			return resetInterrupts;
-		}; 
+    unsigned short inline GetInterruptStatus() {
+      return (interruptStatus & InterruptStatusRegisterMask);
+    }; 
 
-		unsigned int inline GetClearInterruptsAddress() {
-			return interruptsClear;
-		};
+    unsigned short inline GetInterruptGlobalEnable() {
+      return interruptConfigValue & InterruptConfigGlobalEnableMask;
+    }; 
 
-		unsigned int inline GetGateTimeWordLowAddress() { 
-			return gateTimeWordLowAddress;
-		}; 
-                unsigned int inline GetGateTimeWordHighAddress() {
-			return gateTimeWordHighAddress;
-		}; 
+    unsigned short inline GetInterruptConfig() {
+      return interruptConfigValue;
+    }; 
 
-		unsigned int inline GetSequencerResetRegister() {
-			return sequencerResetRegister;
-		} 
+    unsigned int inline GetInterruptsConfigAddress() {
+      return interruptConfig;
+    }; 
+
+    unsigned int inline GetInterruptStatusAddress() {
+      return interruptStatusRegister;
+    }; 
+
+    unsigned short inline GetClearInterrupts() {
+      return resetInterrupts;
+    }; 
+
+    unsigned int inline GetClearInterruptsAddress() {
+      return interruptsClear;
+    };
+
+    unsigned int inline GetGateTimeWordLowAddress() { 
+      return gateTimeWordLowAddress;
+    }; 
+    unsigned int inline GetGateTimeWordHighAddress() {
+      return gateTimeWordHighAddress;
+    }; 
+
+    unsigned int inline GetSequencerResetRegister() {
+      return sequencerResetRegister;
+    } 
 
 
-		/*! control stuff */
-		void SetCRCEnable(bool a);
-		void SetSendEnable(bool a);
-		void SetReTransmitEnable(bool a);
-		unsigned short inline GetControlRegister() {return controlRegisterAddress;};
-		long_m inline GetStatusRegisterAddress() {return statusRegisterAddress;}; 
-		unsigned int inline GetClearRegisterAddress() {return clearStatusRegister;};
+    /*! control stuff */
+    void SetCRCEnable(bool a);
+    void SetSendEnable(bool a);
+    void SetReTransmitEnable(bool a);
+    unsigned short inline GetControlRegister() {return controlRegisterAddress;};
+    long_m inline GetStatusRegisterAddress() {return statusRegisterAddress;}; 
+    unsigned int inline GetClearRegisterAddress() {return clearStatusRegister;};
 };
 
 #endif
--- minervadaq/fermihardware/src/CRIM.cpp
+++ minervadaq/fermihardware/src/CRIM.cpp
@@ -7,12 +7,12 @@
 #include "exit_codes.h"
 
 /*********************************************************************************
-* Class for creating Chain Read-Out Controller Interfact Module objects for 
-* use with the MINERvA data acquisition system and associated software projects.
-*
-* Elaine Schulte, Rutgers University
-* Gabriel Perdue, The University of Rochester
-**********************************************************************************/
+ * Class for creating Chain Read-Out Controller Interfact Module objects for 
+ * use with the MINERvA data acquisition system and associated software projects.
+ *
+ * Elaine Schulte, Rutgers University
+ * Gabriel Perdue, The University of Rochester
+ **********************************************************************************/
 
 log4cpp::Category& CRIMLog = log4cpp::Category::getInstance(std::string("CRIM"));
 
@@ -55,39 +55,39 @@ const unsigned short CRIM::softCNRSTseq   = 0x0808;
 CRIM::CRIM( unsigned int address, log4cpp::Appender* appender, Controller* controller ) :
   VMECommunicator( address, appender, controller )
 {
-	this->addressModifier = cvA24_U_DATA; 
+  this->addressModifier = cvA24_U_DATA; 
 
-	// NOTE: The IRQ level must be the same as the configuration register level.  
-	// The BIT MASKS for these levels, however are not the same!
-	irqLine = SGATEFall;           // default, but configurable
-	irqLevel             = cvIRQ5; // interrupt level 5 for the CAEN interrupt handler
-	interruptConfigValue = 5;      // the interrupt level stored in the interrupt config register
-	resetInterrupts      = 0x81;   // the value to clear all pending interrupts
+  // NOTE: The IRQ level must be the same as the configuration register level.  
+  // The BIT MASKS for these levels, however are not the same!
+  irqLine              = SGATEFall; // correct choice for every running mode but Cosmics
+  irqLevel             = cvIRQ5;    // interrupt level 5 for the CAEN interrupt handler
+  interruptConfigValue = 5;         // the interrupt level stored in the interrupt config register
+  resetInterrupts      = 0x81;      // clear all pending interrupts
 
-	CRIMAppender = appender;
+  CRIMAppender = appender;
   if (CRIMAppender == 0 ) {
     std::cout << "CRIM Log Appender is NULL!" << std::endl;
     exit(EXIT_CRIM_UNSPECIFIED_ERROR);
   }
   CRIMLog.setPriority(log4cpp::Priority::DEBUG); 
 
-	interruptStatusRegister = this->address + (unsigned int)CRIMInterruptStatus;
-	interruptConfig         = this->address + (unsigned int)CRIMInterruptConfig;
-	interruptsClear         = this->address + (unsigned int)CRIMClearInterrupts;
-	interruptAddress        = this->address + (unsigned int)CRIMInterruptMask;
-	timingRegister          = this->address + (unsigned int)CRIMTimingSetup;
-	SGATEWidthRegister      = this->address + (unsigned int)CRIMSGATEWidth;
-	TCALBDelayRegister      = this->address + (unsigned int)CRIMTCALBDelay;
-	softwareTriggerRegister = this->address + (unsigned int)CRIMSoftwareTrigger;
-	softwareTCALBRegister   = this->address + (unsigned int)CRIMSoftwareTCALB;
-	softwareSGATERegister   = this->address + (unsigned int)CRIMSoftwareSGATE;
-	softwareCNRSTRegister   = this->address + (unsigned int)CRIMSoftwareCNRST;
-	controlRegisterAddress  = this->address + (unsigned int)CRIMControl;
-	statusRegisterAddress   = this->address + (unsigned int)CRIMStatus;
-	clearStatusRegister     = this->address + (unsigned int)CRIMClearStatus;
-	gateTimeWordLowAddress  = this->address + (unsigned int)CRIMGateTimeWordLow;
-	gateTimeWordHighAddress = this->address + (unsigned int)CRIMGateTimeWordHigh;
-	sequencerResetRegister  = this->address + (unsigned int)CRIMSequencerControlLatch;
+  interruptStatusRegister = this->address + (unsigned int)CRIMInterruptStatus;
+  interruptConfig         = this->address + (unsigned int)CRIMInterruptConfig;
+  interruptsClear         = this->address + (unsigned int)CRIMClearInterrupts;
+  interruptAddress        = this->address + (unsigned int)CRIMInterruptMask;
+  timingRegister          = this->address + (unsigned int)CRIMTimingSetup;
+  SGATEWidthRegister      = this->address + (unsigned int)CRIMSGATEWidth;
+  TCALBDelayRegister      = this->address + (unsigned int)CRIMTCALBDelay;
+  softwareTriggerRegister = this->address + (unsigned int)CRIMSoftwareTrigger;
+  softwareTCALBRegister   = this->address + (unsigned int)CRIMSoftwareTCALB;
+  softwareSGATERegister   = this->address + (unsigned int)CRIMSoftwareSGATE;
+  softwareCNRSTRegister   = this->address + (unsigned int)CRIMSoftwareCNRST;
+  controlRegisterAddress  = this->address + (unsigned int)CRIMControl;
+  statusRegisterAddress   = this->address + (unsigned int)CRIMStatus;
+  clearStatusRegister     = this->address + (unsigned int)CRIMClearStatus;
+  gateTimeWordLowAddress  = this->address + (unsigned int)CRIMGateTimeWordLow;
+  gateTimeWordHighAddress = this->address + (unsigned int)CRIMGateTimeWordHigh;
+  sequencerResetRegister  = this->address + (unsigned int)CRIMSequencerControlLatch;
 
   // register value for control register (DAQ Mode control)
   // set crc & send to true and retransmit to false	
@@ -98,9 +98,91 @@ CRIM::CRIM( unsigned int address, log4cpp::Appender* appender, Controller* contr
 //----------------------------------------
 void CRIM::Initialize( RunningModes runningMode )
 {
-  CRIMLog.infoStream() << "Initializing CRIM 0x" << std::hex << this->address 
-    << " for Running Mode " << std::dec << runningMode;
+  CRIMLog.infoStream() << "Initializing CRIM 0x" << std::hex << this->address;
+  this->logRunningMode( runningMode );
 
+  // The CRIM will ignore the SequencerEnable instruction in firmware before v9.
+  unsigned short GateWidth         = 0x7F;     // GetWidth must never be zero!
+  unsigned short TCALBDelay        = 0x3FF;    // Delay should also be non-zero.
+  unsigned short TCALBEnable       = 0x1;      // Enable pulse delay.
+  unsigned short SequencerEnable   = 0x1;      // Sequencer control (0 means always send gates, 1 for rearms).
+  CRIMTimingFrequencies Frequency  = ZeroFreq; // Used to set ONE frequency bit!  ZeroFreq ~no Frequency.
+  CRIMTimingModes       TimingMode = MTM;      // Default to MTM.
+
+  switch (runningMode) {
+    case OneShot:
+      // "OneShot" is the casual name for CRIM internal timing with software gates.
+      // We can use MTM mode though and prefer to when an MTM is available. This 
+      // removes the need to switch clock modes for pure pedestal. We keep the name
+      // "OneShot" for historical reasons no matter the clock mode.
+#if NOMTMPEDESTAL 
+      TimingMode   = crimInternal;
+#endif
+      break;
+    case NuMIBeam:
+      break;
+    case PureLightInjection:
+#if MTEST
+      // Because no MTM is available at MTest, LI will use internal timing.
+      TimingMode   = CRIMInternal;
+      CRIMLog.infoStream() << " ->Using MTest timing (CRIM Internal).";
+#endif
+      break;
+    case MixedBeamPedestal:
+    case MixedBeamLightInjection:
+      break;
+    case Cosmics:
+    case MTBFBeamMuon:
+    case MTBFBeamOnly:
+      // Cosmics, Beam-Muon, & Beam-Only use CRIM internal timing with gates send at a set frequency.
+      Frequency    = F2;
+      TimingMode   = crimInternal;
+      this->SetIRQLine(Trigger); //crimInterrupts type
+      break;
+    default:
+      CRIMLog.fatalStream() << "Error in acquire_data::InitializeCrim()! No Running Mode defined!";
+      exit(EXIT_CONFIG_ERROR);
+  }
+  CRIMLog.debugStream() << " GateWidth       = 0x" << std::hex << GateWidth;
+  CRIMLog.debugStream() << " TCALBDelay      = 0x" << std::hex << TCALBDelay;
+  CRIMLog.debugStream() << " TCALBEnable     = 0x" << std::hex << TCALBEnable;
+  CRIMLog.debugStream() << " SequencerEnable = 0x" << std::hex << SequencerEnable;
+  CRIMLog.debugStream() << " Frequency       = " << Frequency;
+  CRIMLog.debugStream() << " Timing Mode     = " << TimingMode;
+
+}
+
+//----------------------------------------
+void CRIM::logRunningMode( RunningModes runningMode )
+{
+  switch (runningMode) {
+    case OneShot:
+      CRIMLog.infoStream() << " Running Mode is OneShot.";
+      break;
+    case NuMIBeam:
+      CRIMLog.infoStream() << " Running Mode is NuMI Beam.";
+      break;
+    case PureLightInjection:
+      CRIMLog.infoStream() << " Running Mode is PureLightInjection.";
+      break;
+    case MixedBeamPedestal:
+      CRIMLog.infoStream() << " Running Mode is MixedBeamPedestal.";
+      break;
+    case MixedBeamLightInjection:
+      CRIMLog.infoStream() << " Running Mode is MixedBeamLightInjection.";
+      break;
+    case Cosmics:
+      CRIMLog.infoStream() << " Running Mode is Cosmic.";
+      break;
+    case MTBFBeamMuon:
+      CRIMLog.infoStream() << " Running Mode is MTBFBeamMuon.";
+      break;
+    case MTBFBeamOnly:
+      CRIMLog.infoStream() << " Running Mode is MTBFBeamOnly.";
+      break;
+    default:
+      CRIMLog.critStream() << "Error in ReadoutWorker::logRunningMode()! Undefined Running Mode!";
+  }
 }
 
 //----------------------------------------
