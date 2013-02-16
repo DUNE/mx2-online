ebc5304275c1ada1a389b60f03c413ee9e53f824
Gabriel Perdue <perdue@fnal.gov>
2013-02-16 11:13:07 -0600

    Move some FEB features into static consts in MinervaDAQtypes.h. Some formatting edits too.

--- minervadaq/fermihardware/include/FEB.h
+++ minervadaq/fermihardware/include/FEB.h
@@ -1,11 +1,6 @@
 #ifndef FEB_h
 #define FEB_h
 
-/* system headers go here */
-
-/* CAEN VME headers go here */
-
-/* custom headers go here */
 #include "trips.h"
 #include "FrameTypes.h"
 #include "Frames.h"
@@ -33,14 +28,13 @@ class FEB : public Frames {
 	private:
 		febAddresses boardNumber;         /*!< the FEB board number (1-15) */
 		unsigned char firmwareVersion[1]; /*!< the firmware version on this board */
-		int maxHits;                      /*!< the maximum number of hits a discriminator can take */
 		int TrueIncomingMessageLength;    /*!< true FEB incoming message length */
 		trips *tripChips[6];              /*!< the trip objects for this FEB */
 		adc *adcHits[8];                  /*!< The ADC objects for this FEB; We can have as many as 8 hits. */
 		disc *hits_n_timing;              /*!< The discriminator for this FEB */
 
-		unsigned char *buffer; /*!< a buffer for FEB data */
-		int NRegisters; /*!< the number of registers this FEB's firmware has */
+		unsigned char *buffer; 
+		int NRegisters; 
 
 		/*! here are some variables for the data about the FEB */
 		unsigned int Timer, TestPulseCount, GateTimeStamp;
@@ -62,7 +56,7 @@ class FEB : public Frames {
 		FEB( febAddresses theAddress ); 
 		~FEB() {
 			for (int i=0;i<6;i++) delete tripChips[i]; 
-			for (int i=0;i<maxHits;i++) delete adcHits[i]; 
+			for (unsigned int i=0;i<ADCFramesMaxNumber;i++) delete adcHits[i]; 
 			delete hits_n_timing;
 		};    
 		// Must clean up FEB outoingMessage arrays manually on a use-by-use basis!
@@ -72,7 +66,7 @@ class FEB : public Frames {
 		/*! Get functions */
 		febAddresses inline GetBoardNumber() {return boardNumber;};
 		int inline GetFirmwareVersion() {return (int)FirmwareVersion[0];};
-		int inline GetMaxHits() {return maxHits;};
+		int inline GetMaxHits() {return ADCFramesMaxNumber;};
 		inline unsigned char *GetBuffer() {return buffer;};
 
 		inline trips *GetTrip(int i) {return tripChips[i];};
@@ -89,66 +83,66 @@ class FEB : public Frames {
 		int inline GetExpectedIncomingMessageLength() {return TrueIncomingMessageLength;};
 
 		/*! Get functions for FPGA read */
-		unsigned int inline   GetTimer() {return Timer;};
-		unsigned short inline GetGateStart() {return GateStart;};
-		unsigned short inline GetGateLength() {return GateLength;};
-    unsigned char inline  GetBoardID() {return boardID[0];};
-		unsigned char inline  GetTripPowerOff() {return TripPowerOff[0];};
-		unsigned char inline  GetInjCount0() {return InjectCount[0][0];};
-		unsigned char inline  GetInjCount1() {return InjectCount[1][0];};
-		unsigned char inline  GetInjCount2() {return InjectCount[2][0];};
-		unsigned char inline  GetInjCount3() {return InjectCount[3][0];};
-		unsigned char inline  GetInjCount4() {return InjectCount[4][0];};
-		unsigned char inline  GetInjCount5() {return InjectCount[5][0];};
-		unsigned char inline  GetInjEnable0() {return InjectEnable[0][0];};
-		unsigned char inline  GetInjEnable1() {return InjectEnable[1][0];};
-		unsigned char inline  GetInjEnable2() {return InjectEnable[2][0];};
-		unsigned char inline  GetInjEnable3() {return InjectEnable[3][0];};
-		unsigned char inline  GetInjEnable4() {return InjectEnable[4][0];};
-		unsigned char inline  GetInjEnable5() {return InjectEnable[5][0];};
-		unsigned char inline  GetInjectRange() {return InjectRange[0];};
-		unsigned char inline  GetInjectPhase() {return InjectPhase[0];};
-		unsigned short inline GetInjDACValue() {return InjectDACValue;};
-		unsigned char inline  GetInjDACMode() {return InjectDACMode[0];};
-		unsigned char inline  GetInjDACStart() {return InjectDACStart[0];};
-		unsigned char inline  GetInjDACDone() {return InjectDACDone[0];};
-		unsigned char inline  GetHVEnabled() {return HVEnabled[0];};
-		unsigned short inline GetHVTarget() {return HVTarget;};
-		unsigned short inline GetHVActual() {return HVActual;};
-		unsigned char inline  GetHVControl() {return HVControl[0];};
-		unsigned char inline  GetHVManual() {return HVManual[0];};
-		unsigned char inline  GetStatusRXLock() {return statusRXLock[0];};
-		unsigned char inline  GetStatusTXSyncLock() {return statusTXSyncLock[0];};
-		unsigned char inline  GetPhaseStart() {return PhaseStart[0];};
-		unsigned char inline  GetPhaseInc() {return PhaseIncrement[0];};
-		unsigned char inline  GetPhaseCount() {return PhaseCount[0];};
-		unsigned char inline  GetStatusSCMDUnknown() {return statusSCMDUnknown[0];};
-		unsigned char inline  GetStatusFCMDUnknown() {return statusFCMDUnknown[0];};
-		unsigned char inline  GetDCM1Lock() {return DCM1Lock[0];};
-		unsigned char inline  GetDCM2Lock() {return DCM2Lock[0];};
-		unsigned char inline  GetDCM1NoClock() {return DCM1NoClock[0];};
-		unsigned char inline  GetDCM2NoClock() {return DCM2NoClock[0];};
-		unsigned char inline  GetDCM2PhaseDone() {return DCM2PhaseDone[0];};
-		unsigned short inline GetDCM2PhaseTotal() {return DCM2PhaseTotal;};
-		unsigned char inline  GetTP2Bit() {return TestPulse2Bit[0];};
-		unsigned int inline   GetTPCount() {return TestPulseCount;};
-		unsigned char inline  GetHVNumAvg() {return HVNumAve[0];};
-		unsigned short inline GetHVPeriodManual() {return HVPeriodManual;};
-		unsigned short inline GetHVPeriodAuto() {return HVPeriodAuto;};
-		unsigned char inline  GetHVPulseWidth() {return HVPulseWidth[0];};
-		unsigned short inline GetTemperature() {return Temperature;};
-		unsigned char inline  GetTripXThresh() {return TripXThresh[0];};
-		unsigned char inline  GetTripXCompEnc() {return TripXCompEnc[0];};
-		unsigned char inline  GetExtTriggerFound() {return ExtTriggerFound[0];};
-		unsigned char inline  GetExtTriggerRearm() {return ExtTriggerRearm[0];};
-		unsigned short inline GetDiscEnMask0() {return DiscrimEnableMask[0];};
-		unsigned short inline GetDiscEnMask1() {return DiscrimEnableMask[1];};
-		unsigned short inline GetDiscEnMask2() {return DiscrimEnableMask[2];};
-		unsigned short inline GetDiscEnMask3() {return DiscrimEnableMask[3];};
-		unsigned int inline   GetGateTimeStamp() {return GateTimeStamp;};
+		const unsigned int inline   GetTimer() const             { return Timer; };
+		const unsigned short inline GetGateStart() const         { return GateStart; };
+		const unsigned short inline GetGateLength() const        { return GateLength; };
+    const unsigned char inline  GetBoardID() const           { return boardID[0]; };
+		const unsigned char inline  GetTripPowerOff() const      { return TripPowerOff[0]; };
+		const unsigned char inline  GetInjCount0() const         { return InjectCount[0][0]; };
+		const unsigned char inline  GetInjCount1() const         { return InjectCount[1][0]; };
+		const unsigned char inline  GetInjCount2() const         { return InjectCount[2][0]; };
+		const unsigned char inline  GetInjCount3() const         { return InjectCount[3][0]; };
+		const unsigned char inline  GetInjCount4() const         { return InjectCount[4][0]; };
+		const unsigned char inline  GetInjCount5() const         { return InjectCount[5][0]; };
+		const unsigned char inline  GetInjEnable0() const        { return InjectEnable[0][0]; };
+		const unsigned char inline  GetInjEnable1() const        { return InjectEnable[1][0]; };
+		const unsigned char inline  GetInjEnable2() const        { return InjectEnable[2][0]; };
+		const unsigned char inline  GetInjEnable3() const        { return InjectEnable[3][0]; };
+		const unsigned char inline  GetInjEnable4() const        { return InjectEnable[4][0]; };
+		const unsigned char inline  GetInjEnable5() const        { return InjectEnable[5][0]; };
+		const unsigned char inline  GetInjectRange() const       { return InjectRange[0]; };
+		const unsigned char inline  GetInjectPhase() const       { return InjectPhase[0]; };
+		const unsigned short inline GetInjDACValue() const       { return InjectDACValue; };
+		const unsigned char inline  GetInjDACMode() const        { return InjectDACMode[0]; };
+		const unsigned char inline  GetInjDACStart() const       { return InjectDACStart[0]; };
+		const unsigned char inline  GetInjDACDone() const        { return InjectDACDone[0]; };
+		const unsigned char inline  GetHVEnabled() const         { return HVEnabled[0]; };
+		const unsigned short inline GetHVTarget() const          { return HVTarget; };
+		const unsigned short inline GetHVActual() const          { return HVActual; };
+		const unsigned char inline  GetHVControl() const         { return HVControl[0]; };
+		const unsigned char inline  GetHVManual() const          { return HVManual[0]; };
+		const unsigned char inline  GetStatusRXLock() const      { return statusRXLock[0]; };
+		const unsigned char inline  GetStatusTXSyncLock() const  { return statusTXSyncLock[0]; };
+		const unsigned char inline  GetPhaseStart() const        { return PhaseStart[0]; };
+		const unsigned char inline  GetPhaseInc() const          { return PhaseIncrement[0]; };
+		const unsigned char inline  GetPhaseCount() const        { return PhaseCount[0]; };
+		const unsigned char inline  GetStatusSCMDUnknown() const { return statusSCMDUnknown[0]; };
+		const unsigned char inline  GetStatusFCMDUnknown() const { return statusFCMDUnknown[0]; };
+		const unsigned char inline  GetDCM1Lock() const          { return DCM1Lock[0]; };
+		const unsigned char inline  GetDCM2Lock() const          { return DCM2Lock[0]; };
+		const unsigned char inline  GetDCM1NoClock() const       { return DCM1NoClock[0]; };
+		const unsigned char inline  GetDCM2NoClock() const       { return DCM2NoClock[0]; };
+		const unsigned char inline  GetDCM2PhaseDone() const     { return DCM2PhaseDone[0]; };
+		const unsigned short inline GetDCM2PhaseTotal() const    { return DCM2PhaseTotal; };
+		const unsigned char inline  GetTP2Bit() const            { return TestPulse2Bit[0]; };
+		const unsigned int inline   GetTPCount() const           { return TestPulseCount; };
+		const unsigned char inline  GetHVNumAvg() const          { return HVNumAve[0]; };
+		const unsigned short inline GetHVPeriodManual() const    { return HVPeriodManual; };
+		const unsigned short inline GetHVPeriodAuto() const      { return HVPeriodAuto; };
+		const unsigned char inline  GetHVPulseWidth() const      { return HVPulseWidth[0]; };
+		const unsigned short inline GetTemperature() const       { return Temperature; };
+		const unsigned char inline  GetTripXThresh() const       { return TripXThresh[0]; };
+		const unsigned char inline  GetTripXCompEnc() const      { return TripXCompEnc[0]; };
+		const unsigned char inline  GetExtTriggerFound() const   { return ExtTriggerFound[0]; };
+		const unsigned char inline  GetExtTriggerRearm() const   { return ExtTriggerRearm[0]; };
+		const unsigned short inline GetDiscEnMask0() const       { return DiscrimEnableMask[0]; };
+		const unsigned short inline GetDiscEnMask1() const       { return DiscrimEnableMask[1]; };
+		const unsigned short inline GetDiscEnMask2() const       { return DiscrimEnableMask[2]; };
+		const unsigned short inline GetDiscEnMask3() const       { return DiscrimEnableMask[3]; };
+		const unsigned int inline   GetGateTimeStamp() const     { return GateTimeStamp; };
 		// new v90 registers...
-		unsigned char inline  GetAfterPulseExtendedWidth() {return AfterPulseExtendedWidth[0];};
-		unsigned char inline  GetPreviewEnable() {return PreviewEnable[0];};
+		const unsigned char inline  GetAfterPulseExtendedWidth() const { return AfterPulseExtendedWidth[0]; };
+		const unsigned char inline  GetPreviewEnable() const           { return PreviewEnable[0]; };
 
 		/*! set functions for FEB setable values */
 		void inline SetTimer(unsigned int a) {Timer=a;};
