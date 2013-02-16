717f0b0dd02ea3e8d3a247ae6dd6b31ed94ce134
Gabriel Perdue <perdue@fnal.gov>
2013-02-16 12:37:15 -0600

    Expand test suite to check some data block sizes when doing charge injection.

--- minervadaq/tests/TestSuite.h
+++ minervadaq/tests/TestSuite.h
@@ -7,11 +7,20 @@
 #include "Controller.h"
 #include "ECROC.h"
 
-unsigned char * ReadDPMTest( ECROC * ecroc, unsigned int channel, unsigned int nFEBs );
+// These two tests are coupled and should be called back-to-back. 
+// We use the pointer from one test in the other.
+unsigned char * ReadDPMTestData( ECROC * ecroc, unsigned int channel, 
+    unsigned int nFEBs, unsigned short pointer );
+unsigned short int ReadDPMTestPointer( ECROC * ecroc, unsigned int channel, 
+    unsigned int nFEBs );
+
 void SetupChargeInjection( EChannels* channel, unsigned int nFEBs );
+void SetupGenericFEBSettings( EChannels* channel, unsigned int nFEBs );
 void FPGAWriteConfiguredFrame( EChannels* channel, FEB* feb );
-void TRIPSetupForChargeInjection( EChannels* channel, int boardID );
 void FPGASetupForChargeInjection( EChannels* channel, int boardID );
+void TRIPSetupForChargeInjection( EChannels* channel, int boardID );
+void FPGASetupForGeneric( EChannels* channel, int boardID );
+void TRIPSetupForGeneric( EChannels* channel, int boardID );
 void TestChannel( ECROC* ecroc, unsigned int channelNumber, unsigned int nFEBs );
 ECROC * GetAndTestECROC( unsigned int address, Controller * controller );
 Controller * GetAndTestController( int address, int crateNumber );
