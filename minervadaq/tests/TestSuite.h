8f01f0e1bab445e369640c762d8aeac9f4c60355
Gabriel Perdue <perdue@fnal.gov>
2013-02-27 14:07:34 -0600

    Test the new SequencerReadoutBlock class.

--- minervadaq/tests/TestSuite.h
+++ minervadaq/tests/TestSuite.h
@@ -9,6 +9,7 @@
 #include "Controller.h"
 #include "ECROC.h"
 #include "CRIM.h"
+#include "SequencerReadoutBlock.h"
 
 #include "ReadoutWorker.h"
 
@@ -39,14 +40,18 @@ unsigned short int ReadDPMTestPointer( ECROC * ecroc, unsigned int channel,
     unsigned int nFEBs );
 
 // This test should be called eigth.
+void SequencerReadoutBlockTest( unsigned char * data, unsigned short dataLength );
+
+// This test should be called ninth.
 void ReadADCTest( EChannels* channel, unsigned int nFEBs );
 
 // This test should be called tenth.
 void ReadDiscrTest( EChannels* channel, unsigned int nFEBs );
 
-// This test should be called ninth.
+// This test should be called eleventh.
 CRIM * GetAndTestCRIM( unsigned int address, Controller * controller );
 
+// This test should be called twelfth.
 ReadoutWorker * GetAndTestReadoutWorker( int controllerID, unsigned int ecrocCardAddress, 
     unsigned int crimCardAddress, int nch0, int nch1, int nch2, int nch3);
 
