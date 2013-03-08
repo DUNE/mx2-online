e3bc6362366bbee310c0ed978f239672615a8c15
Gabriel Perdue <perdue@fnal.gov>
2013-03-08 16:51:54 -0600

    Add a new class to represent a VME Crate. In the future, Readout Worker will hold a vector of Crates, and DAQ Worker will hold only one Readout Worker.

---
+++ minervadaq/fermihardware/include/VMECrate.h
@@ -0,0 +1,49 @@
+#ifndef VMECrate_h
+#define VMECrate_h
+
+#include "log4cppHeaders.h"
+
+#include "VMEModuleTypes.h"
+#include "Controller.h" 
+#include "ECROC.h"
+#include "CRIM.h"
+#include "SequencerReadoutBlock.h"
+
+#include "ReadoutTypes.h"
+
+class VMECrate {
+
+	friend std::ostream& operator<<(std::ostream&, const VMECrate&);
+
+	private:
+		Controller *controller;
+    std::vector<ECROC*> ecrocs;
+    std::vector<CRIM*>  crims;
+
+    int crateID;  // == crate ID/Address for Controller
+		bool vmeInit;    
+    RunningModes runningMode;
+
+	public:
+		explicit VMECrate( int theCrateID, log4cpp::Priority::Value priority, bool VMEInit=false );
+		~VMECrate();
+
+    void AddECROC( unsigned int address, int nFEBchan0=11, int nFEBchan1=11, int nFEBchan2=11, int nFEBchan3=11 );
+    void AddCRIM( unsigned int address );
+    void Initialize( RunningModes runningMode );
+
+		void SendSoftwareRDFE() const;
+		void WaitForSequencerReadoutCompletion() const;
+		void EnableSequencerReadout() const; 
+		void DisableSequencerReadout() const;  
+    void ClearAndResetStatusRegisters() const;
+    void OpenGateFastCommand() const;
+
+    std::vector<ECROC*>* GetECROCVector();
+    ECROC* GetECROCVector( int index );
+
+    std::vector<CRIM*>* GetCRIMVector();
+    CRIM* GetCRIMVector( int index );
+
+};
+#endif
---
+++ minervadaq/fermihardware/src/VMECrate.cpp
@@ -0,0 +1,179 @@
+#ifndef VMECrate_cxx
+#define VMECrate_cxx
+
+#include "VMECrate.h"
+
+log4cpp::Category& vmeCrate = log4cpp::Category::getInstance(std::string("vmeCrate")); 
+
+//---------------------------
+VMECrate::VMECrate( int theCrateID, log4cpp::Priority::Value priority, bool VMEInit ) :
+  crateID(theCrateID),
+  vmeInit(VMEInit),
+  runningMode((RunningModes)0)
+{
+  vmeCrate.setPriority(priority);
+
+  controller = new Controller(0x00, crateID);
+  int error = controller->Initialize();
+  if ( 0 != error ) {
+    vmeCrate.fatalStream() << "Controller contact error: " << error 
+      << "; for Crate ID = " << crateID; 
+    exit(error);
+  }
+  vmeCrate.debugStream() << "Made new ReadoutWokrer with crate ID = " << crateID 
+    << "; VME Init = " << vmeInit
+    << "; Logging Level = " << priority;
+}
+
+//---------------------------
+VMECrate::~VMECrate() {
+  for( std::vector<ECROC*>::iterator p=ecrocs.begin(); p!=ecrocs.end(); ++p ) {
+    delete (*p);
+  }
+  ecrocs.clear();
+  for( std::vector<CRIM*>::iterator p=crims.begin(); p!=crims.end(); ++p ) {
+    delete (*p);
+  }
+  crims.clear();
+  delete controller; 
+}
+
+//---------------------------
+void VMECrate::Initialize( RunningModes theRunningMode )
+{
+  runningMode = theRunningMode;
+  vmeCrate.debugStream() << "Initialize " << (*this);
+
+  vmeCrate.debugStream() << "Initializing " << crims.size() << " CRIMs...";
+  for( std::vector<CRIM*>::iterator p=crims.begin(); p!=crims.end(); ++p ) {
+    (*p)->Initialize( runningMode );
+  }
+  vmeCrate.debugStream() << "Initializing " << ecrocs.size() << " CROC-Es...";
+  for( std::vector<ECROC*>::iterator p=ecrocs.begin(); p!=ecrocs.end(); ++p ) {
+    (*p)->Initialize();
+  }
+	// IRQ Enable will happen in the ReadoutWorker, which holds the proper MasterCRIM*
+  /* vmeCrate.debugStream() << "Enabling IRQ for master CRIM."; */
+  /* this->MasterCRIM()->IRQEnable(); */
+  /* vmeCrate.debugStream() << "Finished Crate Initialization for " << (*this); */
+}
+
+//---------------------------
+void VMECrate::AddECROC( unsigned int address, int nFEBchan0, int nFEBchan1, int nFEBchan2, int nFEBchan3 )
+{
+  if (address < (1<<VMEModuleTypes::ECROCAddressShift)) {
+    address = address << VMEModuleTypes::ECROCAddressShift;
+  }
+  vmeCrate.infoStream() << "Adding ECROC with address = 0x" 
+    << std::hex << address << " and FEBs-to-Channel of (" 
+    << std::dec << nFEBchan0 << ", " << nFEBchan1 << ", " << nFEBchan2 << ", " << nFEBchan3 << ")";
+  if (nFEBchan0<0 || nFEBchan0>10) nFEBchan0 = 0;
+  if (nFEBchan1<0 || nFEBchan1>10) nFEBchan1 = 0;
+  if (nFEBchan2<0 || nFEBchan2>10) nFEBchan2 = 0;
+  if (nFEBchan3<0 || nFEBchan3>10) nFEBchan3 = 0;
+
+  ECROC *theECROC = new ECROC( address, this->controller );
+  theECROC->ClearAndResetStatusRegisters();
+  theECROC->DisableSequencerReadout(); // make sure this is disabled for init
+  vmeCrate.debugStream() << " Adding FEBs to Channels...";
+  theECROC->GetChannel( 0 )->SetupNFrontEndBoards( nFEBchan0 );
+  vmeCrate.debugStream() << " Setup Channel 0 with " << nFEBchan0 << " FEBS.";
+  theECROC->GetChannel( 1 )->SetupNFrontEndBoards( nFEBchan1 );
+  vmeCrate.debugStream() << " Setup Channel 1 with " << nFEBchan1 << " FEBS.";
+  theECROC->GetChannel( 2 )->SetupNFrontEndBoards( nFEBchan2 );
+  vmeCrate.debugStream() << " Setup Channel 2 with " << nFEBchan2 << " FEBS.";
+  theECROC->GetChannel( 3 )->SetupNFrontEndBoards( nFEBchan3 );
+  vmeCrate.debugStream() << " Setup Channel 3 with " << nFEBchan3 << " FEBS.";
+  theECROC->ClearEmptyChannels();
+  ecrocs.push_back( theECROC );
+  vmeCrate.debugStream() << "Added ECROC.";
+}
+
+//---------------------------
+void VMECrate::AddCRIM( unsigned int address )
+{
+  if (address < (1<<VMEModuleTypes::CRIMAddressShift)) {
+    address = address << VMEModuleTypes::CRIMAddressShift;
+  }
+  vmeCrate.infoStream() << "Adding CRIM with address = 0x" << std::hex << address; 
+  CRIM* crim = new CRIM( address, this->controller );
+  vmeCrate.debugStream() << " CRIM Status = 0x" << std::hex << crim->GetStatus();
+  crims.push_back( crim );
+  vmeCrate.debugStream() << "Added CRIM.";
+}
+
+//---------------------------
+void VMECrate::SendSoftwareRDFE() const
+{
+  for (std::vector<ECROC*>::const_iterator p=ecrocs.begin(); p!=ecrocs.end(); ++p) 
+    (*p)->SendSoftwareRDFE();
+}
+
+//---------------------------
+void VMECrate::WaitForSequencerReadoutCompletion() const
+{
+  for (std::vector<ECROC*>::const_iterator p=ecrocs.begin(); p!=ecrocs.end(); ++p) 
+    (*p)->WaitForSequencerReadoutCompletion();
+}
+
+//---------------------------
+void VMECrate::EnableSequencerReadout() const
+{
+  for (std::vector<ECROC*>::const_iterator p=ecrocs.begin(); p!=ecrocs.end(); ++p) 
+    (*p)->EnableSequencerReadout();
+}
+
+//---------------------------
+void VMECrate::DisableSequencerReadout() const
+{
+  for (std::vector<ECROC*>::const_iterator p=ecrocs.begin(); p!=ecrocs.end(); ++p) 
+    (*p)->DisableSequencerReadout();
+}
+
+//---------------------------
+void VMECrate::ClearAndResetStatusRegisters() const
+{
+  for (std::vector<ECROC*>::const_iterator p=ecrocs.begin(); p!=ecrocs.end(); ++p) 
+    (*p)->ClearAndResetStatusRegisters();
+}
+
+//---------------------------
+void VMECrate::OpenGateFastCommand() const
+{
+  for (std::vector<ECROC*>::const_iterator p=ecrocs.begin(); p!=ecrocs.end(); ++p) 
+    (*p)->FastCommandOpenGate();
+}
+
+//---------------------------
+std::vector<ECROC*>* VMECrate::GetECROCVector()
+{
+	return &ecrocs;
+}
+
+//---------------------------
+ECROC* VMECrate::GetECROCVector( int index )
+{
+	return ecrocs[index];
+}
+
+//---------------------------
+std::vector<CRIM*>* VMECrate::GetCRIMVector()
+{
+	return &crims;
+}
+
+//---------------------------
+CRIM* VMECrate::GetCRIMVector( int index )
+{
+	return crims[index];
+}
+
+//-----------------------------
+std::ostream& operator<<(std::ostream& out, const VMECrate& s)
+{
+	out << "Crate = " << s.crateID << "; ";
+	out << "Running Mode = " << s.runningMode << "; ";
+	return out;
+}
+
+#endif
