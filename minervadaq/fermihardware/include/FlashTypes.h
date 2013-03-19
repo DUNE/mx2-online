9d54a5c526d30d68c3eaec356988898252b06fe9
Gabriel Perdue <gnperdue@me.com>
2013-03-19 10:40:02 -0500

    Doygen updates.

--- minervadaq/fermihardware/include/FlashTypes.h
+++ minervadaq/fermihardware/include/FlashTypes.h
@@ -1,21 +1,32 @@
 #ifndef FlashTypes_h
 #define FlashTypes_h
+/*! \file FlashTypes.h
+*/
 
-//typecast to unsigned char
-typedef enum OpCodes { 
+/*!
+  \brief Constants required to program the FEB Flash memory.
+  \author Elaine Schulte
+  */
+
+namespace Flash {
+
+  //typecast to unsigned char
+  typedef enum OpCodes { 
     opcMainMemoryProgramPageThroughBuffer1 = 0x82,
     opcMainMemoryProgramPageThroughBuffer2 = 0x85,
     opcMainMemoryPageRead = 0xD2,
     opcStatusRegisterRead = 0xD7
-};
+  };
 
 
-//typecast to unsigned char
-typedef enum OpCodeByteCounts { 
+  //typecast to unsigned char
+  typedef enum OpCodeByteCounts { 
     opcbMainMemoryPageRead = 8,
     opcbMainMemoryProgramPageThroughBuffer1 = 4,
     opcbMainMemoryProgramPageThroughBuffer2 = 4,
     opcbStatusRegisterRead = 1
-};
+  };
+
+}
 
 #endif
