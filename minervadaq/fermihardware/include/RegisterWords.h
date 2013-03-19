21bbc1fd4254da299c7349711419733023f3ae5c
Gabriel Perdue <gnperdue@me.com>
2013-03-19 10:40:02 -0500

    Doygen updates.

--- minervadaq/fermihardware/include/RegisterWords.h
+++ minervadaq/fermihardware/include/RegisterWords.h
@@ -1,8 +1,18 @@
+#ifndef RegisterWords_h
+#define RegisterWords_h
+/*! \file RegisterWords.h
+*/
+
+/*!
+  \brief Constants used for specific messages to the VME modules.
+  \author Gabriel Perdue
+  */
 
 namespace RegisterWords {
 
-  unsigned char channelReset[] = {0x00,0x80};
-  unsigned char sendMessage[]  = {0x00,0x40};
-  
+  unsigned char channelReset[] = {0x00,0x80};  /*!< Send to a CROC-E Channel */
+  unsigned char sendMessage[]  = {0x00,0x40};  /*!< Send to a CROC-E Channel */
 
 };
+
+#endif
