b3aba39716e0cf01894cfe6cf750b0bcfdc48950
Gabriel Perdue <perdue@fnal.gov>
2013-03-22 14:32:29 -0500

    Split the tests file in two - one that does valgrind and one that doesn't.

---
+++ minervadaq/tests/testMemory.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+EXE=${DAQROOT}/bin/tests
+ARGS="-c 2 -h 1 -f 5"
+VALLOG="grindtest1.txt"
+
+# ${EXE} ${ARGS}
+
+if test -e ${VALLOG}; then
+  rm ${VALLOG}
+fi
+valgrind --num-callers=50 --leak-check=full --verbose --show-reachable=yes --suppressions=${DAQROOT}/tests/suppressions02.supp --log-file-exactly=${VALLOG} ${EXE} ${ARGS}
+# valgrind --num-callers=50 --leak-check=full --verbose --show-reachable=yes --gen-suppressions=all --log-file-exactly=${VALLOG} ${EXE} ${ARGS}
+
+cp $VALLOG leaksum.txt
+perl -i -e 'while(<>) { chomp; if (/definitely/) { print $_; } }' leaksum.txt
+cat leaksum.txt
+echo 
+
