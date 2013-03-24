68d24c9dad596a85f4fc6ec0e18558160c012f31
Gabriel Perdue <perdue@fnal.gov>
2013-03-24 14:46:48 -0500

    Expand reporting powers of the custom exception class. Mildly annoyed what() is not a const method or does not have a const version. (Making me think about not inheriting from std::exception anymore... But, still inheriting for now.)

--- minervadaq/fermihardware/src/FHWException.cpp
+++ minervadaq/fermihardware/src/FHWException.cpp
@@ -26,30 +26,38 @@ FHWException::FHWException( std::string theMessage ) :
 
 //-----------------------------
 FHWException::FHWException( 
-    int crateNumber, 
-    FrameTypes::FEBAddresses febAddress, 
+    int theCrateNumber, 
+    FrameTypes::FEBAddresses theFebAddress, 
     unsigned int channelAddress, 
     std::string theMessage ) :
-  message(theMessage)
+  message(theMessage),
+  crateNumber(theCrateNumber),
+  febAddress(theFebAddress),
+  vmeType(VMEModuleTypes::UnknownCommunicator),
+  address(channelAddress)
 {
   constructorHelper();
   excpLog.errorStream() << "FHWException for crate " << crateNumber << 
     "; FEB Address = " << febAddress << "; channel Address = 0x" << std::hex <<
-    channelAddress;
+    address;
 }
 
 //-----------------------------
 FHWException::FHWException( 
-    int crateNumber, 
-    VMEModuleTypes::VMECommunicatorType vmeType,
+    int theCrateNumber, 
+    VMEModuleTypes::VMECommunicatorType theVmeType,
     unsigned int vmeAddress, 
     std::string theMessage ) :
-  message(theMessage)
+  message(theMessage),
+  crateNumber(theCrateNumber),
+  febAddress(FrameTypes::febAll),
+  vmeType(theVmeType),
+  address(vmeAddress)
 {
   constructorHelper();
   excpLog.errorStream() << "FHWException for crate " << crateNumber << 
     "; VME Module Type = " << vmeType << "; VME Address = 0x" << std::hex <<
-    vmeAddress;
+    address;
 }
 
 //-----------------------------
@@ -60,9 +68,33 @@ void FHWException::constructorHelper()
 }
 
 //-----------------------------
-const char * FHWException::what() 
+const char * FHWException::what()
 { 
   return message.c_str();
+}
+
+//-----------------------------
+int FHWException::getCrate() const
+{
+  return crateNumber;
+}
+
+//-----------------------------
+FrameTypes::FEBAddresses FHWException::getFEBAddress() const
+{
+  return febAddress;
+}
+
+//-----------------------------
+VMEModuleTypes::VMECommunicatorType FHWException::getVMECommunicatorType() const
+{
+  return vmeType;
+}
+
+//-----------------------------
+unsigned int FHWException::getVMEAddress() const
+{
+  return address;
 }
 
 #endif
