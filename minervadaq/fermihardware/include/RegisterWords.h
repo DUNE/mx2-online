e1f83ef5bba3c5a4bc2e31658b916bcfe539c1e8
Gabriel Perdue <perdue@fnal.gov>
2013-03-28 14:05:10 -0500

    Expand the set of pre-programmed byte-array form messages for the E-CROC channel command register.

--- minervadaq/fermihardware/include/RegisterWords.h
+++ minervadaq/fermihardware/include/RegisterWords.h
@@ -4,13 +4,25 @@
 */
 
 /*!
-  \brief Constants used for specific messages to the VME modules.
+  \brief Pre-processed constants used for specific messages to the VME modules.
   \author Gabriel Perdue
+
+	Essentially, these messages are shorts transformed into byte arrays for 
+	convenience in function calls. 
   */
 namespace RegisterWords {
 
-  unsigned char channelReset[] = {0x00,0x80};  /*!< Send to a ECROC EChannels */
-  unsigned char sendMessage[]  = {0x00,0x40};  /*!< Send to a ECROC EChannels */
+	// Process the short messages into byte-swapped words for direct use by the 
+	// messaging function. Be careful of what is using a swapped data width and 
+	// what isn't. (It is better to be consistent than "right.")
+  /* Prototype: unsigned char word[] = { (wordShort & 0xFF), ((wordShort>>8) & 0xFF) };  */
+
+  unsigned char channelReset[]       = {0x00,0x80};  /*!< Clear Status; Send to a ECROC EChannels Command Register */
+  unsigned char sendMessage[]        = {0x00,0x40};  /*!< Send Message; Send to a ECROC EChannels Command Register */
+  unsigned char resetSendMemory[]    = {0x00,0x20};  /*!< Send to a ECROC EChannels Command Register */
+  unsigned char resetReceiveMemory[] = {0x00,0x10};  /*!< Send to a ECROC EChannels Command Register */
+  unsigned char clearEventCounter[]  = {0x00,0x08};  /*!< Send to a ECROC EChannels Command Register */
+  unsigned char sendSyncPatterns[]   = {0x01,0x00};  /*!< Send to a ECROC EChannels Command Register */
 
 };
 
