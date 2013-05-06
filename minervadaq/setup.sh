bcd42137d64684801555a0867a7e89e6e6663a8d
William Badgett <badgett@minervatest04.fnal.gov>
2013-05-06 14:02:13 -0500

    Remove redundant line

--- minervadaq/setup.sh
+++ minervadaq/setup.sh
@@ -4,6 +4,5 @@ export LOCALE=FNAL
 
 SETUP_DIR=`dirname ${BASH_SOURCE[0]}`
 export DAQROOT=`readlink -f ${SETUP_DIR}`
-export DAQROOT=${HOME}/minervadaq/minervadaq
 export DAQSCRIPT=${DAQROOT}/setupdaqenv.sh
 source $DAQSCRIPT $DAQROOT
