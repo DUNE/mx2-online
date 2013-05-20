cf600de133fda1ba034dd54c53ec645ed4f5de25
William Badgett <badgett@minervatest04.fnal.gov>
2013-05-20 14:38:39 -0500

    ET timed changes

--- minervadaq/setup.sh
+++ minervadaq/setup.sh
@@ -5,4 +5,5 @@ export LOCALE=FNAL
 SETUP_DIR=`dirname ${BASH_SOURCE[0]}`
 export DAQROOT=`readlink -f ${SETUP_DIR}`
 export DAQSCRIPT=${DAQROOT}/setupdaqenv.sh
+export CODA_HOME=${DAQROOT}/et-12.0
 source $DAQSCRIPT $DAQROOT
