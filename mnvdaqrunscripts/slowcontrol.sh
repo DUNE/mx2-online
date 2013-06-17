921703899dd27c5fa8ad38c612f621a20b84be2c
Carrie McGivern <mcgivern@fnal.gov>
2013-06-17 14:56:29 -0500

    Update with new path to SlowControl. Will break on Low Energy DAQ machines.

--- mnvdaqrunscripts/slowcontrol.sh
+++ mnvdaqrunscripts/slowcontrol.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-which python2.6 >& /tmp/pytest.txt
-PYV=`perl -ne 'if (/no/) { print "python"; } else { print "python2.6"; }' /tmp/pytest.txt`
-
-pushd /work/software/mnvconfigurator/SlowControl >& /dev/null
-$PYV SC_MainApp.py &
+pushd /work/software/croce/mnv-configurator/SlowControlE2/ >& /dev/null
+python2.6 SC_MainApp.py &
 popd >& /dev/null
+
