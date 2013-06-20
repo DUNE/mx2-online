7827b850f150e9621b79ee8084ad86f30eb713a2
Gabriel Perdue <perdue@fnal.gov>
2013-06-20 14:23:47 -0500

    Updating the .opts stuff a bit. We should remove Make.options by default so we aren't always committing over the top of it...

--- minervadaq/compiler.sh
+++ minervadaq/compiler.sh
@@ -1,60 +1,17 @@
 #!/bin/sh
 
-## single node build
-# if [ $DAQROOT == "/work/software/mnvsingle/mnvdaq" ]; then
-# 	if [ $HOSTNAME == "mnvtbonline0.fnal.gov" ]; then 
-# 		cp ${DAQROOT}/options/mnvtbonline0single.opts $DAQROOT/Make.options
-# 	fi
-# 	if [ $HOSTNAME == "mnvtbonline1.fnal.gov" ]; then 
-# 		cp ${DAQROOT}/options/mnvtbonline1single.opts $DAQROOT/Make.options
-# 	fi
-# 	if [ $HOSTNAME == "minervatest01.fnal.gov" ]; then 
-# 		cp ${DAQROOT}/options/minervatest01single.opts $DAQROOT/Make.options
-# 	fi
-# 	if [ $HOSTNAME == "minervatest02.fnal.gov" ]; then 
-# 		cp ${DAQROOT}/options/minervatest02single.opts $DAQROOT/Make.options
-# 	fi
-# 	if [ $HOSTNAME == "minervatest04.fnal.gov" ]; then 
-# 		cp ${DAQROOT}/options/minervatest04single.opts $DAQROOT/Make.options
-# 	fi
-# 	if [ $HOSTNAME == "mnvonline0.fnal.gov" ]; then 
-# 		cp ${DAQROOT}/options/mnvonline0single.opts $DAQROOT/Make.options
-# 	fi
-# 	if [ $HOSTNAME == "mnvonline1.fnal.gov" ]; then 
-# 		cp ${DAQROOT}/options/mnvonline1single.opts $DAQROOT/Make.options
-# 	fi
-# fi
+# Default to NuMI Setup
+cp ${DAQROOT}/options/numidaq.opts $DAQROOT/Make.options
 
-## multi node build
-# if [ $DAQROOT == "/work/software/mnvonline/mnvdaq" ]; then
-#   if [ $HOSTNAME == "minervatest02.fnal.gov" ]; then 
-#     cp ${DAQROOT}/options/minervatest02multi.opts $DAQROOT/Make.options
-#   fi
-#   if [ $HOSTNAME == "minervatest03.fnal.gov" ]; then 
-#     cp ${DAQROOT}/options/minervatest03multi.opts $DAQROOT/Make.options
-#   fi
-#   if [ $HOSTNAME == "minervatest04.fnal.gov" ]; then 
-#     cp ${DAQROOT}/options/minervatest04multi.opts $DAQROOT/Make.options
-#   fi
-#   if [ $HOSTNAME == "mnvonline0.fnal.gov" ]; then 
-#     cp ${DAQROOT}/options/mnvonline0multi.opts $DAQROOT/Make.options
-#   fi
-#   if [ $HOSTNAME == "mnvonline1.fnal.gov" ]; then 
-#     cp ${DAQROOT}/options/mnvonline1multi.opts $DAQROOT/Make.options
-#   fi
-#   if [ $HOSTNAME == "mnvonline2.fnal.gov" ]; then 
-#     cp ${DAQROOT}/options/mnvonline2crate0multi.opts $DAQROOT/Make.options   # Replace mnvonline0
-#     cp ${DAQROOT}/options/mnvonline2crate1multi.opts $DAQROOT/Make.options   # Replace mnvonline1
-#   fi
-#   if [ $HOSTNAME == "mnvonlinemaster.fnal.gov" ]; then 
-#     cp ${DAQROOT}/options/mnvonlinemastermulti.opts $DAQROOT/Make.options
-#   fi
-#   if [ $HOSTNAME == "mnvonlinebck1.fnal.gov" ]; then 
-#     cp ${DAQROOT}/options/mnvonlinebck1multi.opts $DAQROOT/Make.options
-#   fi
-# fi
+if   [ $LOCALE == "WH14TESTSTAND" ]; then
+  echo "Setting up Wilson Hall 14 SXO Test stand..."
+  cp ${DAQROOT}/options/wh14teststand.opts $DAQROOT/Make.options
+elif [ $LOCALE == "D0TESTSTAND" ]; then
+  echo "Setting up Wilson Hall 14 SXO Test stand..."
+  cp ${DAQROOT}/options/d0teststand.opts $DAQROOT/Make.options
+fi
 
-# nearline builds (always multi node)
+# nearline builds 
 if [ "$HOSTNAME" == "mnvonlinelogger.fnal.gov" ]; then
   cp ${DAQROOT}/options/mnvonlinelogger.opts $DAQROOT/Make.options
 fi
@@ -69,13 +26,6 @@ fi
    gmake relink
  fi
 
-# if [ $HOSTNAME == "mnvonline2.fnal.gov" ]; then 
-#   echo " --- PLEASE NOTE! ---- "
-#   echo " You are building a back-up node that can be used for either VME hardware crate! "
-#   echo " Please check the compiler script to be sure you are set-up for the right hardware "
-#   echo " configuration.  Edit as needed (should be as simple as commenting out a line) and "
-#   echo " recompile as needed. "
-# fi
 
 
 
