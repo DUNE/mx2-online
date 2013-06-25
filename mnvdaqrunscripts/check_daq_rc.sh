8a2706eaa82a6effb6f5c283b40a85f54e00c55a
Gabriel Perdue <perdue@fnal.gov>
2013-06-25 13:25:08 -0500

    Avoid repeated answers.

--- mnvdaqrunscripts/check_daq_rc.sh
+++ mnvdaqrunscripts/check_daq_rc.sh
@@ -4,5 +4,5 @@ ps -leaf | grep python        | grep -v grep
 ps -leaf | grep event_builder | grep -v grep
 ps -leaf | grep et_monitor    | grep -v grep
 ps -leaf | grep et_start      | grep -v grep
-ps -leaf | grep minervadaq    | grep -v grep
+ps -leaf | grep minervadaq    | grep -v grep | grep -v et_start | grep -v et_monitor | grep -v event_builder
 
