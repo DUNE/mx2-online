84882be3e9f90c4c50d4204d19970c674d4cc7e9
Gabriel Perdue <perdue@fnal.gov>
2013-06-28 15:08:13 -0500

    Attempt to rationalize the large number of setup scripts. Put away all but one for now.

--- minervadaq/setupdaqenv.sh
+++ minervadaq/setupdaqenv.sh
@@ -1,8 +1,10 @@
 # Note, to compile the DAQ, after you pull the package down from CVS, you need to untar the ET code first!
 # Set your LOCALE.  One Valid choice right now: "FNAL" for Fermilab.  Currently on the FNAL mnvonline
 # machines, the LOCALE is set in the mnvonline user .bash_profile.
-if [ "$LOCALE" == "" ]
-then
+if [ $# -eq 1 ]; then
+  export LOCALE=$1
+fi
+if [ "$LOCALE" == "" ]; then
   echo No default LOCALE defined!  Assigning your LOCALE to FNAL...
   export LOCALE=FNAL
 fi
@@ -13,13 +15,16 @@ echo
 echo Your LOCALE is $LOCALE
 echo ---------------------------------------------------------------------------
 
-if [ $LOCALE == 'FNAL' ]
-then
+if [ $LOCALE == 'FNAL' ]; then
   export DAQROOT=/work/software/croce/minervadaq/minervadaq
-  export CAEN_DIR=/work/software/CAENVMElib
-  export ET_HOME=$DAQROOT/et_9.0/Linux-x86_64-64
-  export CAEN_VERSION=CAEN_2_7
+  export CODA_VERSION=et_9.0
+  export CODA_HOME=${DAQROOT}/${CODA_VERSION}
+  export BMS_HOME=${CODA_HOME}/BMS
+  export ET_HOME=${CODA_HOME}/Linux-x86_64-64
   export ET_LIBROOT=$ET_HOME/Linux-x86_64-64
+  export INSTALL_DIR=$ET_HOME
+  export CAEN_DIR=/work/software/CAENVMElib
+  export CAEN_VERSION=CAEN_2_7
   # Add $ET_LIBROOT/lib & $CAEN_DIR/lib for ET & CAEN libraries.
   export LD_LIBRARY_PATH=$DAQROOT/lib:$ET_LIBROOT/lib:$CAEN_DIR/lib/x86_64/:$LD_LIBRARY_PATH
   # Add local SQLite
@@ -27,46 +32,72 @@ then
   export PATH=$DAQROOT/sqlite/bin:$PATH
   # Add /usr/local/lib for log4cpp support.
   export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
-elif [ $LOCALE == 'NEARLINE' ]
-then
+elif [ $LOCALE == 'NEARLINE' ]; then
   export DAQROOT=/scratch/nearonline/mirror/minervadaq-croce/minervadaq
   export CAEN_DIR=/scratch/nearonline/mirror/CAENVMElib
-  export ET_HOME=$DAQROOT/et_9.0/Linux-x86_64-64
   export CAEN_VERSION=CAEN_2_7
+  export CODA_VERSION=et_9.0
+  export CODA_HOME=${DAQROOT}/${CODA_VERSION}
+  export BMS_HOME=${CODA_HOME}/BMS
+  export ET_HOME=${CODA_HOME}/Linux-x86_64-64
   export ET_LIBROOT=$ET_HOME/Linux-x86_64-64
+  export INSTALL_DIR=$ET_HOME
   # Add $ET_LIBROOT/lib & $CAEN_DIR/lib for ET & CAEN libraries.
   export LD_LIBRARY_PATH=$DAQROOT/lib:$ET_LIBROOT/lib:$CAEN_DIR/lib/x86_64/:$LD_LIBRARY_PATH
   # Add log4cpp support.
   export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/scratch/nearonline/mirror/log4cpp/lib
-elif [ $LOCALE == "NEARLINEDEV" ]
-then
+elif [ $LOCALE == "NEARLINEDEV" ]; then
   export DAQROOT=/work/mnvdaq
   export CAEN_DIR=/work/CAENVMElib
-  export ET_HOME=$DAQROOT/et_9.0/Linux-x86_64-64
   export CAEN_VERSION=CAEN_2_7
+  export CODA_VERSION=et_9.0
+  export CODA_HOME=${DAQROOT}/${CODA_VERSION}
+  export BMS_HOME=${CODA_HOME}/BMS
+  export ET_HOME=${CODA_HOME}/Linux-x86_64-64
   export ET_LIBROOT=$ET_HOME/Linux-x86_64-64
+  export INSTALL_DIR=$ET_HOME
   # Add $ET_LIBROOT/lib & $CAEN_DIR/lib for ET & CAEN libraries.
   export LD_LIBRARY_PATH=$DAQROOT/lib:$ET_LIBROOT/lib:$CAEN_DIR/lib/x64/:$LD_LIBRARY_PATH
   # Add log4cpp support.
   export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/work/log4cpp/lib
-elif [ $LOCALE == "D0TESTSTAND" ]
-then
+elif [ $LOCALE == "D0TESTSTAND" ]; then
   #export DAQROOT=/work/mnvdaq
   setup caenvme
   export CAEN_DIR=$CAENVME_DIR
   export CAEN_VERSION=CAEN_2_30
-  export ET_HOME=$DAQROOT/et_12.0/Linux-x86_64-64
+  export CODA_VERSION=et_9.0
+  export CODA_HOME=${DAQROOT}/${CODA_VERSION}
+  export BMS_HOME=${CODA_HOME}/BMS
+  export ET_HOME=${CODA_HOME}/Linux-x86_64-64
   export ET_LIBROOT=$ET_HOME/Linux-x86_64-64
+  export INSTALL_DIR=$ET_HOME
   # Add $ET_LIBROOT/lib & $CAEN_DIR/lib for ET & CAEN libraries.
   export LD_LIBRARY_PATH=$DAQROOT/lib:$ET_LIBROOT/lib:$CAEN_DIR/lib/x86_64/:$LD_LIBRARY_PATH
   # Add log4cpp support.
   export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/work/log4cpp/lib
+elif [ $LOCAL == "WH14TESTSTAND" ]; then
+  SETUP_DIR=`dirname ${BASH_SOURCE[0]}`
+  export DAQROOT=`readlink -f ${SETUP_DIR}`
+  export CODA_VERSION=et_9.0
+  export CODA_HOME=${DAQROOT}/${CODA_VERSION}
+  export BMS_HOME=${CODA_HOME}/BMS
+  export ET_HOME=${CODA_HOME}/Linux-x86_64-64
+  export ET_LIBROOT=$ET_HOME/Linux-x86_64-64
+  export INSTALL_DIR=$ET_HOME
+  export CAEN_VERSION=CAEN_2_7
+  export CAEN_DIR=/work/software/CAENVMElib
+  # Add $ET_LIBROOT/lib & $CAEN_DIR/lib for ET & CAEN libraries.
+  export LD_LIBRARY_PATH=$DAQROOT/lib:$ET_LIBROOT/lib:$CAEN_DIR/lib/x86_64/:$LD_LIBRARY_PATH
+  # Add local SQLite
+  export LD_LIBRARY_PATH=$DAQROOT/sqlite/lib:$LD_LIBRARY_PATH
+  export PATH=$DAQROOT/sqlite/bin:$PATH
+  # Add /usr/local/lib for log4cpp support.
+  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
 else
   echo Unsupported LOCALE!
   exit 1
 fi
 
-export BMS_HOME=${DAQROOT}/et_9.0/BMS
 export INSTALL_DIR=$ET_HOME
 export ET_USE64BITS=1
 
