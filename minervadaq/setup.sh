c34fa8857d0e7a018133ec71cd50904f403a87fd
Gabriel Perdue <perdue@fnal.gov>
2013-06-28 15:08:13 -0500

    Attempt to rationalize the large number of setup scripts. Put away all but one for now.

--- minervadaq/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export LOCALE=FNAL
-
-SETUP_DIR=`dirname ${BASH_SOURCE[0]}`
-export DAQROOT=`readlink -f ${SETUP_DIR}`
-export DAQSCRIPT=${DAQROOT}/setupdaqenv.sh
-export CODA_HOME=${DAQROOT}/et-12.0
-source $DAQSCRIPT $DAQROOT
