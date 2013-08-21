e7c39f28f87cc6518228b12d2b321e2515af0428
The MINERvA Online User <mnvonline@mnvonline1.fnal.gov>
2013-08-21 15:38:31 -0500

    Edit to the Trigger() function, so that pedestals and light injection triggering is done the same (so that trigger signals don't overlap) - this is the same problem we saw with the LE croc pedestal running earlier this year.

--- minervadaq/workers/src/ReadoutWorker.cpp
+++ minervadaq/workers/src/ReadoutWorker.cpp
@@ -178,8 +178,8 @@ unsigned long long ReadoutWorker::Trigger( Triggers::TriggerType triggerType )
   switch (triggerType) {
     case Pedestal:        // In pedestal-type modes, we trigger all CRIMs
     case ChargeInjection: // using software.
-      SendSoftwareGate();
-      break;
+      //SendSoftwareGate();
+      //break;
     case LightInjection: // Only trigger master w/software in LI.
       this->MasterCRIM()->SendSoftwareGate(); 
       break;             // In LI and all beam modes, we rely on LEMO connection
