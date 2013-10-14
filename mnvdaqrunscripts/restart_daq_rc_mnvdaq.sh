1551165a5cbfc24142301b5c971af5a93af63caa
Carrie McGivern <mcgivern@fnal.gov>
2013-10-14 14:28:47 -0500

    script to restart processes on mnvonline1 and mnvonlinelogger when the daq freezes up

---
+++ mnvdaqrunscripts/restart_daq_rc_mnvdaq.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# THIS SCRIPT IS A CANDIDATE FOR REMOVAL...
+
+# Kill them all, let God sort them out.
+# -------------------------------------
+# Run this to kill and restart all the DAQ and RC scripts on the mnvonline1 
+# and mnvonlinelogger when running locally or x-forwarding the RC.  
+
+# Get cluster defs.
+. $HOME/mnvdaqrunscripts/defs_mnvonline
+
+# Need to kerberize first. 
+. $HOME/mnvdaqrunscripts/Kerberize
+
+# Kill and Restart the processes on mnvonline1...
+echo "Killing processes on ${MNVDAQ}..."
+`ssh  ${REMDAQACCT}@${MNVDAQ} source ${SCRIPTSDIR}/proc_kill_ALLDAQRC.sh` 
+echo "Wait for it....."
+sleep 5
+echo "Restarting processes on ${MNVDAQ}..."
+`ssh  ${REMDAQACCT}@${MNVDAQ} source ${SCRIPTSDIR}/run_runcontrol_servers.sh`
+echo "Wait for it....."
+sleep 5 
+echo "Killing and Restarting processes on mnvonlinelogger also..."
+`sh dispatcher_nearline.sh`
+sleep 5
+
+# Restart the RC...
+echo "Restarting runcontrol..."
+# Clear any old RC clients.
+$HOME/mnvdaqrunscripts/proc_kill_RunCo.pl
+sleep 5
+. runcontrol.sh
+
+
+
