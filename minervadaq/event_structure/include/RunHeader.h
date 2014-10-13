1b04d33a43511c77e6752acb6fc2b625522bd289
Donatella Torretta <torretta@minervatest04.fnal.gov>
2014-10-13 15:38:59 -0500

    adding new files

---
+++ minervadaq/event_structure/include/RunHeader.h
@@ -0,0 +1,40 @@
+#ifndef RunHeader_h
+#define RunHeader_h
+/*! 
+  \file RunHeader.h
+*/
+
+#include <vector>
+#include "FrameHeader.h"
+
+/*! 
+  \class RunHeader 
+  \brief This class will build the Run Header used to hold run information
+  \author William Badgett
+  */
+class RunHeader 
+{
+  private:
+    unsigned char* data; 
+    unsigned short dataLength;
+
+  public:
+    RunHeader(FrameHeader * header, std::vector<unsigned short> configurations);
+    ~RunHeader();
+
+    enum 
+    {
+      MAX_CROC = 15,
+      CHANNELS_PER_CROC = 4
+    };
+
+    unsigned char* GetData() const;
+    unsigned char GetData(int i) const;
+    unsigned short GetDataLength() const;
+    void ClearData();
+};
+
+
+static const int runHeaderSize = 2 * RunHeader::MAX_CROC * sizeof(unsigned short) + sizeof(int) + FrameHeader::FRAME_HEADER_SIZE * sizeof(short);
+
+#endif
