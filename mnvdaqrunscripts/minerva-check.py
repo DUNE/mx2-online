967372866e1ed0f065c9ae084e1a90820db7689f
William Badgett <badgett@fnal.gov>
2015-07-30 10:01:41 -0500

    Clean up checking

--- mnvdaqrunscripts/minerva-check.py
+++ mnvdaqrunscripts/minerva-check.py
@@ -2,6 +2,7 @@
 
 from xmlrpclib import ServerProxy, Fault
 from time import gmtime, strftime
+import sys
 import os
 import time
 import smtplib
@@ -20,7 +21,7 @@ import datetime
 recentBeamLimit = 2.0
 
 # How recently should triggers have been present?  in seconds
-recentTriggerLimit = 2.0
+recentTriggerLimit = 1.9
 
 #  How many times must DAQ fail to have taken data before alarm?
 alarmFailures = 5
@@ -40,94 +41,96 @@ devices = [ "G:EA9SNC", "G:E00SNC", "G:E2ASNC" ]
 url="http://www-bd.fnal.gov/xmlrpc/Accelerator"
 
 def getError(stat):
-    sp = ServerProxy(url)
-    try:
-        ret = sp.getErrorInfo(stat)
-    except Fault, err:
-        print 'XML-RPC error: '+str(err)
-        sys.exit(1)
-    except socket.error, err:
-        print 'Socket error: '+str(err[1])
-        sys.exit(1)
-    return ret['verbose_text']
+  sp = ServerProxy(url)
+  try:
+    ret = sp.getErrorInfo(stat)
+  except Fault, err:
+    print 'XML-RPC error: '+str(err)
+  except socket.error, err:
+    print 'Socket error: '+str(err[1])
+  return ret['verbose_text']
 
     
 def checkMinerva():
 
-    global failCounter
+  global failCounter
 
-    # First check to see if $A9 is OK
-    serverProxy = ServerProxy(url)
+  # First check to see if $A9 is OK
+  serverProxy = ServerProxy(url)
 
-    try:
-        deviceData = serverProxy.getReading(devices)
-    except Fault, err:
-        print 'XML-RPC error: '+str(err)
-        failCounter = 0
-        return
-    except socket.error, err:
-        print 'Socket error: '+str(err[1])
-        failCounter = 0
-        return
+  try:
+    deviceData = serverProxy.getReading(devices)
+  except Fault, err:
+    print 'XML-RPC error: '+str(err)
+    return
+  except socket.error, err:
+    print 'Socket error: '+str(err[1])
+    return
 
-    # Only consider G:EA9SNC for now
-    reading = deviceData[0]
-    name    = reading['name']
-    data    = float(reading['scaled'])
-    status  = int(reading['status'])
-    units   = reading['units']
-    print "%s %s %f %s %d"%(str(datetime.datetime.now()),name,data,units,status)
-    data = 1.5
-    # Perform check only if we have reasonable data
-    if status == 0 and data > 0.0 and data < recentBeamLimit :
+  # Only consider G:EA9SNC for now
+  reading = deviceData[0]
+  name    = reading['name']
+  data    = float(reading['scaled'])
+  status  = int(reading['status'])
+  units   = reading['units']
+  print "%s %s %f %s %d"%(str(datetime.datetime.now()),name,data,units,status)
+  data = 1.5
+  # Perform check only if we have reasonable data
+  if status == 0 and data > 0.0 and data < recentBeamLimit :
 
-      deviceText = name+" status "+str(status)+" last A9 "+str(data)+" seconds ago "
+    deviceText = name+" status "+str(status)+" last A9 "+str(data)+" seconds ago "
 
-      # Now check for recent Minerva trigger
-      with open(lastTriggerFileName,'r') as triggerFile:
+    # Now check for recent Minerva trigger
+   # try:
+    with open(lastTriggerFileName,'r') as triggerFile:
 
-          for line in triggerFile:
+        for line in triggerFile:
 
-            if line.find('time') >= 0:
-              line = line.strip('\n')
-              zeit = float(line.split('=')[1]) / 1E6
-              zeit = time.time() - 2 
-              deltaT = time.time() - zeit
-              print "Last Trigger %f seconds ago" % deltaT
+          if line.find('time') >= 0:
+            line = line.strip('\n')
+            zeit = float(line.split('=')[1]) / 1E6
+            zeit = time.time() - 2 
+            deltaT = time.time() - zeit
+            print time.strftime("%Y.%m.%d %H:%M:%S")+" Last Trigger %f seconds ago" % deltaT
 
-              if deltaT > recentTriggerLimit:
+            if deltaT > recentTriggerLimit:
  
-                  failCounter = failCounter + 1
-                  print "%f failures so far" % failCounter
-                  if failCounter == alarmFailures:
+              failCounter = failCounter + 1
+              print time.strftime("%Y.%m.%d %H:%M:%S")+" " + str(failCounter) + " failures so far"
+              if failCounter == alarmFailures:
 
-                      message = """MIME-Version: 1.0
+                message = """MIME-Version: 1.0
 Content-type: text/html
 Subject: Minerva Error """+deviceText+"""
-Minerva DAQ not running<BR>"""+deviceText+"""<BR>
-Last trigger """+str(deltaT)+""" seconds ago<BR>"""
-                      print message
-                      try:
-                          smtp = smtplib.SMTP()
-                          smtp.connect();
-                          smtp.sendmail(sender,receivers,message)
-                      except:
-                          print "Failed to sendmail "
-              else:
-                  failCounter = 0
+Minerva DAQ not running """+time.strftime("%Y.%m.%d %H:%M:%S",time.localtime())+"""<BR>"""+deviceText+"""
+<BR>Last trigger """+str(deltaT)+""" seconds ago<BR>"""
+                print message
+                try:
+                  smtp = smtplib.SMTP()
+                  smtp.connect();
+                  smtp.sendmail(sender,receivers,message)
+                except:
+                  print time.strftime("%Y.%m.%d %H:%M:%S",time.localtime())+"Failed to sendmail "
+            else:
+              print "Status OK"
+              failCounter = 0
 
-      triggerFile.close()
-    else:
-      print name+" status "+str(status)+" last A9 "+str(data)+" skip check"
-      failCounter = 0
-      return
+    triggerFile.close()
+    #except:
+      # Race condition with the DAQ sometime, so skip check this time
+     # print "Problem opening file "+lastTriggerFileName +" skip check for now"
+  else:
+    print name+" status "+str(status)+" last A9 "+str(data)+" skip check"
+
 
 def main():
   
   global checkFrequency
 
+  print "Starting minerva-check"
   while 1:
     checkMinerva()
+    sys.stdout.flush()
     time.sleep(checkFrequency)
         
 if __name__ == '__main__':
