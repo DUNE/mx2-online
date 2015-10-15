e4c9290a7b522fdb2b92fc83f07f3816cd7f75f8
cvs-fast-export <cvs-fast-export>
2015-10-15 17:53:48 +0000

    Synthetic commit for incomplete tag savage_libox_om_2015_10_15

--- mnvdaqrunscripts/setup-minerva-cvs.sh
+++
@@ -1,2 +0,0 @@
-export CVS_RSH=ssh
-export CVSROOT=minervacvs@cdcvs.fnal.gov:/cvs/mnvsoft
--- mnvdaqrunscripts/slowcontrol.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-pushd /work/software/mnvconfigurator/SlowControlE2cr2CRC/ >& /dev/null
-python2.6 SC_MainApp.py &
-popd >& /dev/null
-
