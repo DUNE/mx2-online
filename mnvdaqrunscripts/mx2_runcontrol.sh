274189f83a395c898695d3bffc3e08d5209b7f47
Akeem Hart <alhart@FNAL.GOV>
2024-02-21 21:13:40 +0000

    Added mx2 specific run control script and script to set relevant paths

---
+++ mnvdaqrunscripts/mx2_runcontrol.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "PYTHONPATH =" $PYTHONPATH
+
+# Use this script to restart the RunControl GUI on a UROC.
+
+. $HOME/mnvdaqrunscripts/defs_mx2paths
+
+# Need to kerberize first. 
+. $HOME/mnvdaqrunscripts/Kerberize
+
+# First, clear any old RC clients...
+$HOME/mnvdaqrunscripts/proc_kill_RunCo.pl
+
+# Now, start the RC
+pushd ${RCROOT}/frontend
+echo "PYTHONPATH =" $PYTHONPATH
+which python
+python RunControl.py &
+popd
+echo "If you get a socket binding error, just close the RC and wait a minute and then try again."
+echo "If you get a warning about the last subrun not finishing cleanly, just wait for the status "
+echo "bar, and then click okay."
+echo ""
+echo "You may need to exit the run control and run this script again..."
+
