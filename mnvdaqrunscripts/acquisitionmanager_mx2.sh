f3fa09a05f23171ac8c6edd50bffaefd8037c007
Akeem Hart <alhart@FNAL.GOV>
2024-02-22 11:23:38 +0000

    adding acquisitionmanager and dispatcher scripts for mx2

---
+++ mnvdaqrunscripts/acquisitionmanager_mx2.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Use this script to restart the DataAcquisition manager on the mnvonline or 
+# minervatest cluster when running "locally" (either at the terminal or via 
+# ssh'ed x-forwarding) and a single-node DAQ.
+
+. $DAQROOT/../mnvdaqrunscripts/defs_mx2paths
+
+# Setup environment for LinDAQ.
+if test -z "$DAQROOT"
+then
+	echo "No DAQROOT defined.  Sourcing the setup script..."
+	source $HOME/setupdaqenv.sh
+fi
+
+# Get Python version.
+which python >& /tmp/pytest.txt
+#PYV=`perl -ne 'if (/no/) { print "python"; } else { print "python2.6"; }' /tmp/pytest.txt`
+#PYV=/usr/bin/python
+
+# Check to see if the acquisition manager is running.  If it is, stop/kill it.
+pushd ${RCROOT}/backend >& /dev/null
+python DataAcquisitionManager.py stop
+popd >& /dev/null
+
+# Start the dispatcher.
+pushd ${RCROOT}/backend >& /dev/null
+python DataAcquisitionManager.py start
+popd >& /dev/null
+
+ps -leaf | grep DataAcquisitionManager | grep -v grep
