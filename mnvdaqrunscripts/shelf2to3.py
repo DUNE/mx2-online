35142b428febaab590415f89ba020afd6910c454
Akeem Hart <alhart@FNAL.GOV>
2024-02-22 13:09:43 -0600

    Adding script to update db files for python3

---
+++ mnvdaqrunscripts/shelf2to3.py
@@ -0,0 +1,32 @@
+"""
+  shelf2to3.py:
+  Script to regenerate db files produced by shelve in python2 
+  into a db file compatible with python3
+
+  Usage:
+  python shelf2to3.py <python2 .db file> <new .db file>
+  
+   Original author: A. Hart (alhart@fnal.gov)
+                    Feb. 2024
+"""
+import shelve
+import gdbm
+import sys
+
+def gdbm_shelve(filename,flag="c"):
+    return shelve.Shelf(gdbm.open(filename,flag))
+
+def main(argv):
+    out_shelf=gdbm_shelve(argv[1])
+    in_shelf=shelve.open(argv[0])
+
+    key_list=in_shelf.keys()
+    print(key_list)
+    for key in key_list:
+        out_shelf[key]=in_shelf[key]
+
+    out_shelf.close()
+    in_shelf.close()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
