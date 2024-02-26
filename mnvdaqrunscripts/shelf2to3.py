9fe6a5961b0dfa34c057e32f658f7b9f46e5a7d5
Akeem Hart <alhart@FNAL.GOV>
2024-02-26 07:24:56 -0600

    Updates to shelf2to3.py

--- mnvdaqrunscripts/shelf2to3.py
+++ mnvdaqrunscripts/shelf2to3.py
@@ -3,21 +3,23 @@
   Script to regenerate db files produced by shelve in python2 
   into a db file compatible with python3
 
+  Needs to be run in Python2!
+
   Usage:
   To write to new file:
-  python shelf2to3.py <python2 .db file> <new .db file>
+  python2 shelf2to3.py <python2 .db file> <new .db file>
   To overwrite original file:
-  python shelf2to3.py <python2 .db file>
+  python2 shelf2to3.py <python2 .db file>
    Original author: A. Hart (alhart@fnal.gov)
                     Feb. 2024
 """
 import shelve
-import dbm.gnu
+import gdbm
 import sys
 import os
 
 def gdbm_shelve(filename,flag="c"):
-    return shelve.Shelf(dbm.gnu.open(filename,flag))
+    return shelve.Shelf(gdbm.open(filename,flag))
 
 def main(argv):
     outpath = ""
@@ -32,7 +34,8 @@ def main(argv):
     out_shelf=gdbm_shelve(outpath)
     in_shelf=shelve.open(argv[0])
 
-    key_list=list(in_shelf.keys())
+
+    key_list=in_shelf.keys()
     print(key_list)
     for key in key_list:
         out_shelf[key]=in_shelf[key]
@@ -41,6 +44,7 @@ def main(argv):
     in_shelf.close()
     if(len(argv)==1):
         os.replace(outpath, argv[0])
+
 
 if __name__ == "__main__":
    main(sys.argv[1:])
